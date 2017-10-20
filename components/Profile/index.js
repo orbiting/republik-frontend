693732b19ba4df2d21900d69d94d89de51458d54
patte <p@tte.io>
2017-10-20 11:47:59 +0200

    fix profile page for users without a testimonial

--- components/Profile/index.js
+++ components/Profile/index.js
@@ -163,8 +163,10 @@ class Profile extends Component {
                     })
                     : styles.sidebar)}
                 >
-                  <Interaction.H3>{publicUser.testimonial.name}</Interaction.H3>
-                  <div {...styles.role}>{publicUser.testimonial.role}</div>
+                  <Interaction.H3>{publicUser.name}</Interaction.H3>
+                  {publicUser.testimonial && (
+                    <div {...styles.role}>{publicUser.testimonial.role}</div>
+                  )}
 
                   {publicUser.badges && (
                     <div {...styles.badges}>

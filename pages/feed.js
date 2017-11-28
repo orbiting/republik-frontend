1c9ef0357306b867c43e18849fdbcbc9c8b8b019
Daniel Pfänder <daniel.pfaender@project-r.construction>
2017-11-28 18:23:25 +0100

    Feed, a very basic one.

---
+++ pages/feed.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import { compose } from 'redux'
+import Frame from '../components/Frame'
+import Feed from '../components/Feed'
+import Marketing from '../components/Marketing'
+import withData from '../lib/apollo/withData'
+import withMe from '../lib/apollo/withMe'
+import withT from '../lib/withT'
+
+const FeedPage = ({ url, me, t }) => {
+  const meta = {
+    title: t('pages/feed/title')
+  }
+  return (
+    <Frame url={url} meta={meta}>
+      {me && me.id ? <Feed /> : <Marketing />}
+    </Frame>
+  )
+}
+
+export default compose(withData, withMe, withT)(FeedPage)

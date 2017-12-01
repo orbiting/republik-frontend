44c588928b5e8fe538ef361c0027f0088663bcca
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-12-01 19:30:08 +0100

    use newer Loader props and specify error container

--- components/Loader.js
+++ components/Loader.js
@@ -1,9 +1,12 @@
 import React from 'react'
 import { HEADER_HEIGHT } from './constants'
-import { Loader } from '@project-r/styleguide'
+import { Loader, NarrowContainer } from '@project-r/styleguide'
 
 const LoaderWithHeaderHeight = props => (
-  <Loader {...props} height={['100vh', `calc(100vh - ${HEADER_HEIGHT}px)`]} />
+  <Loader
+    {...props}
+    style={{minHeight: ['100vh', `calc(100vh - ${HEADER_HEIGHT}px)`]}}
+    ErrorContainer={NarrowContainer} />
 )
 
 export default LoaderWithHeaderHeight

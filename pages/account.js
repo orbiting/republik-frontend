fdb36b8d68c46a89deded39b4031480911bdaf74
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-12-04 03:33:48 +0100

    port account pledge and membership from cf

--- pages/account.js
+++ pages/account.js
@@ -2,25 +2,25 @@ import React from 'react'
 import { compose } from 'react-apollo'
 import Frame from '../components/Frame'
 import Account from '../components/Account'
-import withAuthorization from '../components/Auth/withAuthorization'
 import withData from '../lib/apollo/withData'
-import withMe from '../lib/apollo/withMe'
 import withT from '../lib/withT'
+import Merci from '../components/Pledge/Merci'
 
-const AccountPage = ({ url, me, t }) => {
+const AccountPage = ({ url, t }) => {
   const meta = {
     title: t('pages/account/title')
   }
+  const postPledge = url.query.id || url.query.claim
   return (
     <Frame url={url} meta={meta}>
-      <Account />
+      {postPledge
+        ? <Merci query={url.query} />
+        : <Account query={url.query} />}
     </Frame>
   )
 }
 
 export default compose(
   withData,
-  withAuthorization(),
-  withMe,
   withT
 )(AccountPage)

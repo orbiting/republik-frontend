27a54776256f1e2249f1d623d88f76bbe68e450c
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-12-05 16:27:24 +0100

    live package list and hot links

--- components/Marketing/Offers.js
+++ components/Marketing/Offers.js
@@ -2,8 +2,10 @@ import React from 'react'
 import withT from '../../lib/withT'
 import { Link } from '../../lib/routes'
 import { css } from 'glamor'
-import { colors, mediaQueries } from '@project-r/styleguide'
+import { colors, mediaQueries, Loader } from '@project-r/styleguide'
 import ChevronRightIcon from 'react-icons/lib/md/chevron-right'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
 
 const styles = {
   offer: css({
@@ -35,35 +37,47 @@ const styles = {
   })
 }
 
-const offers = [
-  {
-    key: 'patron'
-  },
-  {
-    key: 'other'
-  },
-  {
-    key: 'lower'
-  },
-  {
-    key: 'voucher'
-  },
-  {
-    key: 'monthly'
-  }
-]
+const List = ({ t, data }) => (
+  <Loader error={data.error} loading={data.loading} style={{minHeight: 200}} render={() => (
+    <ul {...styles.offer}>
+      {data.crowdfunding.packages
+        .filter(pkg => pkg.name !== 'ABO')
+        .map(pkg => ({
+          key: pkg.name,
+          label: t(`package/${pkg.name}/title`),
+          params: {package: pkg.name}
+        }))
+        .concat([
+          {key: 'claim', label: t('marketing/offers/claim'), route: 'claim'},
+          {key: 'userPrice', label: t('marketing/offers/userPrice'), params: {package: 'ABO', userPrice: 1}}
+        ])
+        .map(({key, label, route = 'pledge', params}) => (
+          <li key={key}>
+            <Link route={route} params={params}>
+              <a>
+                {label}{' '}
+                <ChevronRightIcon size={30} {...styles.icon} />
+              </a>
+            </Link>
+          </li>
+        ))}
+    </ul>
+  )} />
+)
 
-export default withT(({ t }) => (
-  <ul {...styles.offer}>
-    {offers.map(offer => (
-      <li key={offer.key}>
-        <Link route='pledge'>
-          <a>
-            {t(`marketing/offers/${offer.key}`)}{' '}
-            <ChevronRightIcon size={30} {...styles.icon} />
-          </a>
-        </Link>
-      </li>
-    ))}
-  </ul>
-))
+const query = gql`
+query marketingOffers($crowdfundingName: String!) {
+  crowdfunding(name: $crowdfundingName) {
+    id
+    packages {
+      id
+      name
+    }
+  }
+}
+`
+
+export default compose(
+  withT,
+  graphql(query)
+)(List)

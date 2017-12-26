17e56e16fa991997929838562876c863cfc3a5ae
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-12-26 01:31:09 +0100

    workaround for apollo issue

--- components/Profile/UsernameField.js
+++ components/Profile/UsernameField.js
@@ -1,5 +1,5 @@
-import React from 'react'
-import { graphql, compose } from 'react-apollo'
+import React, { Component } from 'react'
+import { compose, withApollo } from 'react-apollo'
 import gql from 'graphql-tag'
 import withT from '../../lib/withT'
 import { errorToString } from '../../lib/utils/errors'
@@ -41,26 +41,72 @@ query checkUsername($value: String) {
 }
 `
 
-const UsernameField = compose(
-  withT,
-  graphql(query, {
-    options: ({value}) => {
-      return {
-        errorPolicy: 'all',
-        variables: {
-          value
-        }
-      }
-    },
-    props: ({ data, ownProps: { t } }) => {
-      return {
-        label: t('profile/username/label'),
-        error: data.error
-          ? errorToString(data.error)
-          : ''
-      }
+class UsernameField extends Component {
+  constructor (...args) {
+    super(...args)
+    this.state = {}
+  }
+  check () {
+    const { client, value } = this.props
+    client
+      .query({
+        query,
+        variables: { value }
+      })
+      .then(({ data }) => {
+        this.setState({error: undefined})
+      })
+      .catch(error => {
+        this.setState({
+          error: errorToString(error)
+        })
+      })
+  }
+  componentWillMount () {
+    this.check()
+  }
+  componentDidUpdate (prevProps) {
+    if (prevProps.value !== this.props.value) {
+      this.check()
     }
-  })
-)(Field)
+  }
+  render () {
+    const { t, client, ...props } = this.props
+    const { error } = this.state
 
-export default UsernameField
+    return <Field
+      label={t('profile/username/label')}
+      error={error}
+      {...props} />
+  }
+}
+
+// TMP: Waiting for fix:
+// - https://github.com/apollographql/apollo-client/issues/2703
+// - once ready: rm class and replace with below
+// const UsernameField = compose(
+//   withT,
+//   graphql(query, {
+//     options: ({value}) => {
+//       return {
+//         errorPolicy: 'all',
+//         variables: {
+//           value
+//         }
+//       }
+//     },
+//     props: ({ data, ownProps: { t } }) => {
+//       return {
+//         label: t('profile/username/label'),
+//         error: data.error
+//           ? errorToString(data.error)
+//           : ''
+//       }
+//     }
+//   })
+// )(Field)
+
+export default compose(
+  withT,
+  withApollo
+)(UsernameField)

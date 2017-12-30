2a295449d997c55038fe62a888b9ba1700cb2ed2
Thomas Preusse <tpreusse@users.noreply.github.com>
2017-12-30 02:50:23 +0100

    add cancel and reactivate for monthly

--- components/Account/Memberships/List.js
+++ components/Account/Memberships/List.js
@@ -1,4 +1,4 @@
-import React, { Component, Fragment } from 'react'
+import React, { Component } from 'react'
 import { compose, graphql } from 'react-apollo'
 
 import withT from '../../../lib/withT'
@@ -7,21 +7,13 @@ import {
   Interaction, Loader
 } from '@project-r/styleguide'
 
-import { Item as AccountItem, P } from '../Elements'
-
-import { timeFormat } from '../../../lib/utils/format'
-
 import query from '../belongingsQuery'
 
-const {H2} = Interaction
+import Manage from './Manage'
 
-const dayFormat = timeFormat('%d. %B %Y')
+const { H2 } = Interaction
 
 class MembershipsList extends Component {
-  constructor (props) {
-    super(props)
-    this.state = {}
-  }
   render () {
     const {
       memberships, t,
@@ -39,40 +31,11 @@ class MembershipsList extends Component {
             <H2>{t.pluralize('memberships/title', {
               count: memberships.length
             })}</H2>
-            {memberships.map(membership => {
-              const createdAt = new Date(membership.createdAt)
-              const latestPeriod = membership.periods[0]
-              const formattedEndDate = dayFormat(new Date(latestPeriod.endDate))
-
-              return (
-                <AccountItem key={membership.id}
-                  highlighted={highlightId === membership.pledge.id}
-                  createdAt={createdAt}
-                  title={[
-                    t(
-                      `memberships/type/${membership.type.name}`,
-                      {},
-                      membership.type.name
-                    ),
-                    `(${t('memberships/sequenceNumber/suffix', membership)})`
-                  ].join(' ')}>
-                  <P>
-                    {membership.active && !membership.overdue && t.first(
-                      [
-                        `memberships/${membership.type.name}/latestPeriod/renew/${membership.renew}`,
-                        `memberships/latestPeriod/renew/${membership.renew}`
-                      ],
-                      { formattedEndDate },
-                      ''
-                    )}
-                    {membership.overdue && t(
-                      'memberships/latestPeriod/overdue',
-                      { formattedEndDate }
-                    )}
-                  </P>
-                </AccountItem>
-              )
-            })}
+            {memberships.map(membership => (
+              <Manage key={membership.id}
+                membership={membership}
+                highlighted={highlightId === membership.pledge.id} />
+            ))}
           </div>
         )
       }} />

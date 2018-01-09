085857068b6ebc760bb9b2175bd219c65e9f099c
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-01-09 03:36:59 +0100

    pay notes for format

--- components/Feed/Format.js
+++ components/Feed/Format.js
@@ -2,12 +2,18 @@ import React from 'react'
 import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
 
-import { Center, TeaserFeed, Loader } from '@project-r/styleguide'
+import { Center, TeaserFeed, Interaction, Loader } from '@project-r/styleguide'
 import Link from '../Link/Href'
+
+import withT from '../../lib/withT'
+
+import Box from '../Frame/Box'
+import { WithoutMembership } from '../Auth/withMembership'
 
 const getFeedDocuments = gql`
 query getFeedDocuments($formatId: String!) {
   documents(format: $formatId) {
+    totalCount
     nodes {
       meta {
         credits
@@ -21,13 +27,24 @@ query getFeedDocuments($formatId: String!) {
 }
 `
 
-const Feed = ({ data: { loading, error, documents } }) => (
+const Feed = ({ t, data: { loading, error, documents } }) => (
   <Loader
     loading={loading}
     error={error}
     render={() => {
       return (
         <Center>
+          <Interaction.H2>
+            {t.pluralize('format/feed/title', {count: documents.totalCount})}
+          </Interaction.H2>
+          <br /><br />
+          <WithoutMembership render={() => (
+            <Box style={{padding: '15px 20px'}}>
+              <Interaction.P>
+                {t('format/feed/payNote')}
+              </Interaction.P>
+            </Box>
+          )} />
           {documents &&
             documents.nodes.map(doc => (
               <TeaserFeed
@@ -43,5 +60,6 @@ const Feed = ({ data: { loading, error, documents } }) => (
 )
 
 export default compose(
+  withT,
   graphql(getFeedDocuments)
 )(Feed)

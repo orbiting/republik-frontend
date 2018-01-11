35070119dfcac70f2c04d4aa3a6ca8fb3bf23732
Pascal Kaufmann <kaufmae@me.com>
2018-01-11 15:23:02 +0100

    first draft

--- components/Auth/TokenAuthorization.js
+++ components/Auth/TokenAuthorization.js
@@ -1,29 +1,41 @@
 import React from 'react'
 import { compose } from 'react-apollo'
 import {
-  Button
+  Button, P, Label, H2, H1
 } from '@project-r/styleguide'
 
 import withUnauthorizedSession from '../../lib/apollo/withUnauthorizedSession'
 import withT from '../../lib/withT'
 
-const TokenAuthorization = ({ t, unauthorizedSession: { country, city, ipAddress, userAgent } = {}, email, token }) => (
-  <div>
-    {userAgent && (
-      <div>
-        {country}
-        {city}
-        {ipAddress}
-        {userAgent}
-        {email}
+const TokenAuthorization = ({ t, unauthorizedSession, email, token }) => {
+  const { country, city, ipAddress, userAgent, countryFlag } = unauthorizedSession || {}
+  return (
+    <React.Fragment>
+      <H1>Sign-in Request</H1>
+      <P>
+        Somebody wants to sign in at republik.ch with your e-mail address {email || ''}. If that's not you, contact us at support@republik.ch
+      </P>
+      {ipAddress && (
+        <React.Fragment>
+          <div>
+            <H2>Location of origin:</H2>
+            <H1>{countryFlag}</H1>
+            <Label>{country || 'Unknown Location'}</Label>
+            <Label>{city || ''}</Label>
 
-        <Button primary onClick={() => {}}>
-          That's fine
-        </Button>
-      </div>
-    )}
-  </div>
-)
+            <H2>Device</H2>
+            <div>{ipAddress}</div>
+            <Label>{userAgent}</Label>
+          </div>
+          <br />
+          <Button primary onClick={() => {}}>
+            Authorize
+          </Button>
+        </React.Fragment>
+      )}
+    </React.Fragment>
+  )
+}
 
 export default compose(
   withUnauthorizedSession,

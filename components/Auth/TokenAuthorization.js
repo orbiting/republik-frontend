cbf1e60644a113b316ae28e70857121dec4f031f
Pascal Kaufmann <kaufmae@me.com>
2018-01-11 16:19:12 +0100

    refactor to preusse style 🤓

--- components/Auth/TokenAuthorization.js
+++ components/Auth/TokenAuthorization.js
@@ -1,35 +1,29 @@
 import React from 'react'
-import { compose } from 'react-apollo'
-import {
-  Button, P, Label, H2, H1
-} from '@project-r/styleguide'
-
-import withUnauthorizedSession from '../../lib/apollo/withUnauthorizedSession'
+import { graphql, compose } from 'react-apollo'
+import gql from 'graphql-tag'
+import { Button, P, Label, H2, H1 } from '@project-r/styleguide'
 import withT from '../../lib/withT'
 
 const TokenAuthorization = ({ t, unauthorizedSession, email, token }) => {
   const { country, city, ipAddress, userAgent, countryFlag } = unauthorizedSession || {}
   return (
     <React.Fragment>
-      <H1>Sign-in Request</H1>
-      <P>
-        Somebody wants to sign in at republik.ch with your e-mail address {email || ''}. If that's not you, contact us at support@republik.ch
-      </P>
+      <H1>{t('notifications/authorization/title')}</H1>
+      <P>{t('notifications/authorization/text', { email })}</P>
       {ipAddress && (
         <React.Fragment>
           <div>
-            <H2>Location of origin:</H2>
+            <H2>{t('notifications/authorization/location')}</H2>
             <H1>{countryFlag}</H1>
-            <Label>{country || 'Unknown Location'}</Label>
-            <Label>{city || ''}</Label>
-
-            <H2>Device</H2>
-            <div>{ipAddress}</div>
+            <Label>{country || t('notifications/authorization/location/unknown')}</Label>
+            <Label>{city}</Label>
+            <H2>{t('notifications/authorization/device')}</H2>
+            <P>{ipAddress}</P>
             <Label>{userAgent}</Label>
           </div>
           <br />
           <Button primary onClick={() => {}}>
-            Authorize
+            {t('notifications/authorization/button')}
           </Button>
         </React.Fragment>
       )}
@@ -37,7 +31,37 @@ const TokenAuthorization = ({ t, unauthorizedSession, email, token }) => {
   )
 }
 
+const authorizeSession = gql`
+  mutation authorizeSession($email: String!, $token: String!) {
+    authorizeSession(email: $email, token: $token)
+  }
+`
+
+export const unauthorizedSessionQuery = gql`
+  query unauthorizedSession($email: String!, $token: String!) {
+    unauthorizedSession(email: $email, token: $token) {
+      ipAddress
+      userAgent
+      country
+      countryFlag
+      city
+    }
+  }
+`
+
 export default compose(
-  withUnauthorizedSession,
+  graphql(unauthorizedSessionQuery, {
+    props: ({ data }) => {
+      return {
+        unauthorizedSession: data.unauthorizedSession
+      }
+    }
+  }),
+  graphql(authorizeSession, {
+    props: ({ email, token, mutate }) => ({
+      authorize: () =>
+        mutate({ email, token })
+    })
+  }),
   withT
 )(TokenAuthorization)

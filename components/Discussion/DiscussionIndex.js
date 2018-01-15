d3c639bc5ac9c045e62c584f6a0ba4c882b790b0
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-01-15 06:36:20 +0100

    clean up discussion overview

--- components/Discussion/DiscussionIndex.js
+++ components/Discussion/DiscussionIndex.js
@@ -1,62 +1,27 @@
-import React, {PureComponent} from 'react'
+import React, { Fragment } from 'react'
 import { graphql, compose } from 'react-apollo'
 import gql from 'graphql-tag'
-import {H1, H2, A, Field, Button} from '@project-r/styleguide'
+import { A, Interaction } from '@project-r/styleguide'
 import { Link } from '../../lib/routes'
 import withT from '../../lib/withT'
 
 import HrefLink from '../Link/Href'
+import Loader from '../Loader'
+import Meta from '../Frame/Meta'
+import List, { Item } from '../List'
 
-class DiscussionIndex extends PureComponent {
-  constructor (props) {
-    super(props)
-
-    this.state = {
-      title: '',
-      maxLength: undefined,
-      minInterval: undefined
-    }
-
-    this.createDiscussion = () => {
-      const {title, maxLength, minInterval} = this.state
-      this.props.createDiscussion(title, maxLength, minInterval)
-      this.setState({title: '', maxLength: undefined, minInterval: undefined})
-    }
-  }
-
-  render () {
-    const {t, data: {loading, error, discussions}} = this.props
-    const {title, maxLength, minInterval} = this.state
-
-    if (loading || error) {
-      return null
-    } else {
-      return (
-        <div>
-          <H1>{t('discussion/pageTitle')}</H1>
-
-          <H2>Neue Diskussion erstellen</H2>
-          <div>
-            <Field
-              label='Titel'
-              value={title}
-              onChange={(_, title) => this.setState({title})}
-            />
-            <Field
-              label='Maximale länge'
-              value={maxLength}
-              onChange={(_, maxLength) => this.setState({maxLength: +maxLength})}
-            />
-            <Field
-              label='Minimales interval (sekunden)'
-              value={minInterval}
-              onChange={(_, minInterval) => this.setState({minInterval: +minInterval})}
-            />
-            <Button primary disabled={title === ''} onClick={this.createDiscussion}>Erstellen</Button>
-          </div>
-
-          <H2>Alle Diskussionen</H2>
-          {discussions.map((d, i) => {
+const DiscussionIndex = ({t, data: {loading, error, discussions}}) => (
+  <Loader
+    loading={loading}
+    error={error}
+    render={() => (
+      <Fragment>
+        <Interaction.H1>
+          {t('discussion/pageTitle')}
+        </Interaction.H1>
+        <Meta data={{title: t('discussion/pageTitle')}} />
+        <List>
+          {discussions.filter(d => d.title).map((d, i) => {
             const children = <A>{d.title || d.id}</A>
             let link
             if (d.documentPath) {
@@ -69,16 +34,15 @@ class DiscussionIndex extends PureComponent {
               </Link>
             }
             return (
-              <div key={i}>
+              <Item key={i}>
                 {link}
-              </div>
+              </Item>
             )
           })}
-        </div>
-      )
-    }
-  }
-}
+        </List>
+      </Fragment>
+    )} />
+)
 
 const discussionsQuery = gql`
 query discussions {
@@ -91,27 +55,6 @@ query discussions {
 `
 
 export default compose(
-withT,
-graphql(discussionsQuery),
-graphql(gql`
-mutation createDiscussion($title: String!, $maxLength: Int, $minInterval: Int) {
-  createDiscussion(title: $title, maxLength: $maxLength, minInterval: $minInterval, anonymity: ALLOWED)
-}
-`, {
-  props: ({mutate}) => ({
-    createDiscussion: (title, maxLength, minInterval) => {
-      mutate({
-        variables: {
-          title,
-          maxLength,
-          minInterval: minInterval === undefined ? undefined : minInterval * 1000
-        },
-        update: (proxy, {data: {createDiscussion}}) => {
-          const data = proxy.readQuery({query: discussionsQuery})
-          data.discussions.push({__typename: 'Discussion', id: createDiscussion, title})
-          proxy.writeQuery({query: discussionsQuery, data})
-        }
-      })
-    }
-  })
-}))(DiscussionIndex)
+  withT,
+  graphql(discussionsQuery)
+)(DiscussionIndex)

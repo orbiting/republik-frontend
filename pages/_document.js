7c94a9eb284c4ca73641112e0639d7418ade8877
Diego Muracciole <diegomuracciole@gmail.com>
2018-06-04 12:34:25 -0300

    Disable page zooming on mobile

--- pages/_document.js
+++ pages/_document.js
@@ -36,7 +36,7 @@ export default class MyDocument extends Document {
         <Head>
           <meta
             name='viewport'
-            content='width=device-width,initial-scale=1'
+            content='width=device-width,initial-scale=1,maximum-scale=1.0,user-scalable=0'
           />
           <meta
             httpEquiv='X-UA-Compatible'

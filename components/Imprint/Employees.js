a05d7de7c74087a9101c4d826efb1f63526eb87c
Daniel Pfänder <daniel.pfaender@project-r.construction>
2018-07-10 10:51:12 +0200

    adapt employees to better backend API and finally take down crew page

--- components/Imprint/Employees.js
+++ components/Imprint/Employees.js
@@ -61,109 +61,57 @@ const styles = {
   })
 }
 
-const getEmployeesGsheet = gql`
+const getEmployees = gql`
   query {
     employees {
-      userId
+      title
+      name
       group
       subgroup
-      name
-      title
-    }
-  }
-`
-
-const getEmployeesData = gql`
-  query getEmployeesData($ids: [ID!]) {
-    search(filter: { type: User, ids: $ids }, first: 200) {
-      nodes {
-        entity {
-          ... on User {
-            id
-            portrait
-            username
-          }
-        }
+      user {
+        id
+        portrait
+        username
       }
     }
   }
 `
 
-const EmployeeList = compose(
-  graphql(getEmployeesData, {
-    options: props => ({
-      variables: {
-        ids: props.ids
-      }
-    })
-  })
-)(({ employeeGroups, data: { loading, error, search } }) => {
-  return (
-    <Loader
-      loading={loading}
-      error={error}
-      render={() => {
-        const { nodes } = search
-
-        const userById = nest()
-          .key(d => d['entity']['id'])
-          .rollup(d => d[0]['entity'])
-          .object(nodes)
-
-        const renderEmployee = (employee, i) =>
-          <Employee
-            {...employee}
-            {...userById[employee.userId]}
-            key={i}
-          />
-
-        return (
-          <div {...styles.container}>
-            {
-              entries(employeeGroups).map(group =>
-                <section {...styles.group} key={group.key}>
-                  <H2 {...styles.groupHeading}>{group.key}</H2>
-                  { group.value.group
-                    ? group.value.group.map(renderEmployee)
-                    : entries(group.value).map(subgroup => (
-                      <section {...styles.subgroup} key={subgroup.key}>
-                        <H3 {...styles.subgroupHeading}>{subgroup.key}</H3>
-                        {subgroup.value.map(renderEmployee)}
-                      </section>
-                    ))
-                  }
-                </section>
-              )
-            }
-          </div>
-        )
-      }}
-    />
-  )
-})
+const renderEmployee = (employee, i) => (
+  <Employee {...employee} key={i} />
+)
 
 const Employees = compose(
-  graphql(getEmployeesGsheet, {})
+  graphql(getEmployees, {})
 )(({ data: { loading, error, employees } }) => (
   <Loader
     loading={loading}
     error={error}
     render={() => {
-      const uniqueUserIds = [
-        ...new Set(
-          employees.map(employee => {
-            return employee.userId
-          })
-        )
-      ].filter(Boolean)
-
       const employeeGroups = nest()
         .key(d => d['group'])
         .key(d => d['subgroup'] || 'group')
         .object(employees)
 
       return (
-        <EmployeeList ids={uniqueUserIds} employeeGroups={employeeGroups} />
+        <div {...styles.container}>
+          {
+            entries(employeeGroups).map(group =>
+              <section {...styles.group} key={group.key}>
+                <H2 {...styles.groupHeading}>{group.key}</H2>
+                { group.value.group
+                  ? group.value.group.map(renderEmployee)
+                  : entries(group.value).map(subgroup => (
+                    <section {...styles.subgroup} key={subgroup.key}>
+                      <H3 {...styles.subgroupHeading}>{subgroup.key}</H3>
+                      {subgroup.value.map(renderEmployee)}
+                    </section>
+                  ))
+                }
+              </section>
+            )
+          }
+        </div>
       )
     }}
   />

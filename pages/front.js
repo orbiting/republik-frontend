ecfe202f394d5d6c53965fea86ac7a4c53741f83
Lukas Buenger <lukasbuenger@gmail.com>
2018-08-02 10:57:40 +0200

    cleanup imports/arguments

--- pages/front.js
+++ pages/front.js
@@ -2,15 +2,13 @@ import React from 'react'
 import { compose } from 'react-apollo'
 import Front from '../components/Front'
 import withData from '../lib/apollo/withData'
-import withT from '../lib/withT'
 import { enforceMembership } from '../components/Auth/withMembership'
 
-const FrontPage = ({ url, t, isMember, headers }) => {
+const FrontPage = ({ url, headers }) => {
   return <Front url={url} headers={headers} />
 }
 
 export default compose(
   withData,
-  enforceMembership,
-  withT
+  enforceMembership
 )(FrontPage)

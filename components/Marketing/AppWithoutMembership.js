fc6653ab1d1354375404b09df1f7c338ffa49f79
Lukas Buenger <lukasbuenger@gmail.com>
2018-08-21 14:17:14 +0200

    show AppWithoutMembership if is app and no membership

---
+++ components/Marketing/AppWithoutMembership.js
@@ -0,0 +1,60 @@
+import React, { Fragment} from 'react'
+import { compose } from 'react-apollo'
+import { Link } from '../../lib/routes'
+import withMe from '../../lib/apollo/withMe'
+import withT from '../../lib/withT'
+import { css } from 'glamor'
+
+import {
+  Container,
+  Editorial,
+  linkRule,
+  mediaQueries
+} from '@project-r/styleguide'
+
+const MAX_WIDTH = '1005px'
+
+// TODO: revisit special font sizes with design.
+const styles = {
+  container: css({
+    paddingBottom: 60,
+    [mediaQueries.mUp]: {
+      paddingBottom: 120
+    }
+  }),
+  noMember: css({
+    textAlign: 'center',
+    padding: '18px 0',
+    [mediaQueries.mUp]: {
+      padding: '30px 0'
+    }
+  })
+}
+
+const AppIndex = ({ me, t, crowdfundingName, data }) => (
+  <Fragment>
+    <div {...styles.container}>
+      {me && (
+        <div {...styles.noMember}>
+          <Container style={{ maxWidth: MAX_WIDTH }}>
+            <Editorial.P>
+              {t.elements('marketing/noActiveMembership', {
+                link: (
+                  <Link route='account' key='account'>
+                    <a {...linkRule}>{t('marketing/noActiveMembership/link')}</a>
+                  </Link>
+                )
+              })}
+            </Editorial.P>
+          </Container>
+        </div>
+      )}
+
+    </div>
+  </Fragment>
+)
+
+export default compose(
+  withMe,
+  withT
+)(AppIndex)

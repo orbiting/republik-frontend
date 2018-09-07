d6a1e9b2a0d406f5b99993aa547f1de02ca2f171
Lukas Buenger <lukasbuenger@gmail.com>
2018-09-07 17:15:23 +0200

    use Box to style beforeNote on preview front

--- components/Marketing/Preview.js
+++ components/Marketing/Preview.js
@@ -1,34 +1,21 @@
 import React, {Fragment} from 'react'
 import { compose } from 'react-apollo'
-import {css} from 'glamor'
+
 import {
   A,
   Container,
-  Interaction,
-  mediaQueries,
-  colors
+  Interaction
 } from '@project-r/styleguide'
 
 import { Link } from '../../lib/routes'
-import Frame, { Box } from '../Frame'
+import Frame from '../Frame'
+import Box from '../Frame/Box'
 import SignUp from './SignUp'
 import Front from '../Front'
 
 import withT from '../../lib/withT'
 import withMe from '../../lib/apollo/withMe'
 import withMembership from '../Auth/withMembership'
-
-const styles = {
-  noMember: css({
-    margin: '0 -15px',
-    backgroundColor: colors.primaryBg,
-    textAlign: 'center',
-    padding: '18px 0',
-    [mediaQueries.mUp]: {
-      padding: '30px 0'
-    }
-  })
-}
 
 const Prestitial = ({ me, isMember, t }) => {
   const text = me && !isMember
@@ -38,11 +25,11 @@ const Prestitial = ({ me, isMember, t }) => {
     )
     : t('marketing/preview/prestitial/withMembership')
 
-  return <div {...styles.noMember}>
-    <Interaction.P>
+  return (
+    <Interaction.P style={{ textAlign: 'center' }}>
       {text}
     </Interaction.P>
-  </div>
+  )
 }
 
 const Preview = ({ me, isMember, url, meta, t }) => {

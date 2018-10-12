179338216b9d0c8eae34528c31d56ff93f3456f7
Andreas Moor <amoor@fastmail.net>
2018-10-12 13:13:42 +0200

    temporarily remove withData

--- pages/voteDiscuss.js
+++ pages/voteDiscuss.js
@@ -1,11 +1,9 @@
 import ElectionDiscussionPage from '../components/Vote/ElectionDiscussionPage'
-import withData from '../lib/apollo/withData'
 import { compose } from 'react-apollo'
 import { enforceMembership } from '../components/Auth/withMembership'
 import withMe from '../lib/apollo/withMe'
 
 export default compose(
-  withData,
   enforceMembership,
   withMe
 )(ElectionDiscussionPage)

b24147f3274c0faa2eee5a8dcc933adc06a27387
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-10-24 10:47:24 +0200

    clean up

--- components/Link/utils.js
+++ components/Link/utils.js
@@ -1,9 +1,12 @@
 export const shouldIgnoreClick = event => {
   // source https://github.com/zeit/next.js/blob/82d56e063aad12ac8fee5b9d5ed24ccf725b1a5b/packages/next-server/lib/link.js#L59
   const { nodeName, target } = event.currentTarget
-  if (nodeName === 'A' &&
-    ((target && target !== '_self') || event.metaKey || event.ctrlKey || event.shiftKey || (event.nativeEvent && event.nativeEvent.which === 2))) {
-    // ignore click for new tab / new window behavior
-    return true
-  }
+  return nodeName === 'A' &&
+    !!(
+      (target && target !== '_self') ||
+      event.metaKey ||
+      event.ctrlKey ||
+      event.shiftKey ||
+      (event.nativeEvent && event.nativeEvent.which === 2)
+    )
 }

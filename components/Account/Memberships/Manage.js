7881ed096be280b68270da5f4931e71b5ed995f3
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-11-13 15:50:39 +0100

    give context about automatic monthly cancel on buying a yearly

--- components/Account/Memberships/Manage.js
+++ components/Account/Memberships/Manage.js
@@ -5,12 +5,13 @@ import gql from 'graphql-tag'
 import withT from '../../../lib/withT'
 import { errorToString } from '../../../lib/utils/errors'
 import { timeFormat } from '../../../lib/utils/format'
+import { Link } from '../../../lib/routes'
 
 import { Item as AccountItem, P, A } from '../Elements'
 import FieldSet from '../../FieldSet'
 
 import {
-  Button, InlineSpinner, colors
+  Button, InlineSpinner, colors, linkRule, Interaction
 } from '@project-r/styleguide'
 
 const dayFormat = timeFormat('%d. %B %Y')
@@ -50,9 +51,17 @@ class Manage extends Component {
         {membership.active &&
           !isCancelling &&
           membership.type.name === 'MONTHLY_ABO' &&
-          <A style={{ display: 'block' }} href='/angebote?package=ABO'>
-            {t(`memberships/${membership.type.name}/manage/upgrade/link`)}
-          </A>}
+          <P>
+            <Interaction.Cursive>
+              {t.elements('memberships/MONTHLY_ABO/manage/upgrade/link', {
+                buyLink: <Link route='pledge' params={{ package: 'ABO' }}>
+                  <a {...linkRule}>
+                    {t('memberships/MONTHLY_ABO/manage/upgrade/link/buyText')}
+                  </a>
+                </Link>
+              })}
+            </Interaction.Cursive>
+          </P>}
         {membership.active && membership.renew && !isCancelling &&
           <A href='#cancel' onClick={(e) => {
             e.preventDefault()

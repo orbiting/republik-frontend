d7abb23b6dfa0df6b8beb8caab32a0cadc15fb33
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-11-24 02:45:32 +0100

    prolong box v0

---
+++ components/Link/TokenPackage.js
@@ -0,0 +1,37 @@
+import React from 'react'
+import { compose, graphql } from 'react-apollo'
+import gql from 'graphql-tag'
+
+import withInNativeApp from '../../lib/withInNativeApp'
+import { Link } from '../../lib/routes'
+
+const tokenQuery = gql`
+query accessTokenCustomPledge {
+  me {
+    id
+    accessToken(scope: CUSTOM_PLEDGE)
+  }
+}
+`
+
+const TokenPackageLink = compose(
+  withInNativeApp,
+  graphql(tokenQuery, {
+    skip: props => !props.inNativeApp,
+    props: ({ data }) => ({
+      loading: data.loading,
+      accessToken: data.me && data.me.accessToken
+    })
+  })
+)(({ loading, accessToken, params, ...props }) => {
+  if (loading) {
+    return '...'
+  }
+  const p = { ...params }
+  if (accessToken) {
+    p.token = accessToken
+  }
+  return <Link route='pledge' params={p} {...props} />
+})
+
+export default TokenPackageLink

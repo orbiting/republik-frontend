ee05244396733fd5c7969fd1a7ef846c7d94fb58
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-12-25 18:34:32 +0100

    teaser block with scrubber

--- pages/overview.js
+++ pages/overview.js
@@ -5,7 +5,6 @@ import { css } from 'glamor'
 import { withRouter } from 'next/router'
 
 import { nest } from 'd3-collection'
-// import { lab } from 'd3-color'
 
 import { swissTime } from '../lib/utils/format'
 import { ASSETS_SERVER_BASE_URL, RENDER_FRONTEND_BASE_URL } from '../lib/constants'
@@ -53,6 +52,8 @@ const SIZES = [
   { minWidth: 810, columns: 8 }
 ]
 
+const PADDING = 10
+
 const styles = {
   container: css({
     display: 'flex',
@@ -63,8 +64,8 @@ const styles = {
     width: '100%'
   }),
   item: css({
-    paddingBottom: 10,
-    paddingRight: 10,
+    paddingBottom: PADDING,
+    paddingRight: PADDING,
     lineHeight: 0,
     ...SIZES.reduce((styles, size) => {
       const width = `${100 / size.columns}%`
@@ -77,7 +78,199 @@ const styles = {
       }
       return styles
     }, {})
+  }),
+  hoverArea: css({
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    bottom: 0,
+    right: 0
   })
+}
+
+class TeaserBlock extends Component {
+  constructor (props, ...args) {
+    super(props, ...args)
+    this.blockRef = React.createRef()
+    this.hoverRef = React.createRef()
+    this.state = {}
+  }
+  measure = () => {
+    const parent = this.blockRef.current
+    if (!parent) {
+      return
+    }
+
+    const { left, top, width } = parent.getBoundingClientRect()
+    if (this.state.width !== width) {
+      this.setState({ width })
+    }
+
+    this.measurements = Array.from(
+      parent.querySelectorAll('[data-teaser]')
+    ).map(teaser => {
+      const rect = teaser.getBoundingClientRect()
+
+      return {
+        id: teaser.getAttribute('data-teaser'),
+        x: rect.left - left,
+        y: rect.top - top,
+        height: rect.height,
+        width: rect.width
+        // absoluteY: window.pageYOffset + rect.top
+      }
+    })
+  }
+  componentDidMount () {
+    window.addEventListener('resize', this.measure)
+    this.hoverRef.current.addEventListener('touchstart', this.focus, { passive: false })
+    this.hoverRef.current.addEventListener('touchmove', this.focus)
+    this.hoverRef.current.addEventListener('touchend', this.blur)
+    this.measure()
+  }
+  componentDidUpdate () {
+    this.measure()
+  }
+  componentWillUnmount () {
+    if (this.hoverRef.current) {
+      this.hoverRef.current.removeEventListener('touchstart', this.focus)
+      this.hoverRef.current.removeEventListener('touchmove', this.focus)
+      this.hoverRef.current.removeEventListener('touchend', this.blur)
+    }
+    window.removeEventListener('resize', this.measure)
+  }
+  focus = (event) => {
+    const parent = this.blockRef.current
+    if (!parent || !this.measurements) {
+      return
+    }
+    const { left, top } = parent.getBoundingClientRect()
+
+    let hoverTouch = false
+    let currentEvent = event
+    if (currentEvent.changedTouches) {
+      hoverTouch = true
+      currentEvent = currentEvent.changedTouches[0]
+    }
+
+    const focusX = currentEvent.clientX - left
+    const focusY = currentEvent.clientY - top
+
+    const measurement = this.measurements.find(teaser => {
+      return (
+        focusX + PADDING >= teaser.x &&
+        focusX <= teaser.x + teaser.width + PADDING &&
+        focusY + PADDING >= teaser.y &&
+        focusY <= teaser.y + teaser.height + PADDING
+      )
+    })
+
+    const hover = measurement
+      ? {
+        measurement,
+        teaser: this.props.teasers.find(t => t.id === measurement.id)
+      }
+      : false
+    if (hover) {
+      event.preventDefault()
+    }
+
+    this.setState({ hover, hoverTouch })
+  }
+  blur = () => {
+    this.setState({ hover: undefined })
+  }
+  render () {
+    const { hover, hoverTouch, width } = this.state
+    const { teasers, lazy } = this.props
+
+    return (
+      <div ref={this.blockRef} style={{ position: 'relative' }}>
+        <LazyLoad visible={!lazy} attributes={{
+          ...styles.container,
+          ...css({
+            ...SIZES.reduce((styles, size) => {
+              const height = teasers.length / size.columns * 66
+              if (size.minWidth) {
+                styles[`@media only screen and (min-width: ${size.minWidth}px)`] = {
+                  height
+                }
+              } else {
+                styles.height = height
+              }
+              return styles
+            }, {})
+          })
+        }}>
+          {teasers.map(teaser => {
+            // `${ASSETS_SERVER_BASE_URL}/render?width=1200&height=1&url=${encodeURIComponent(`${RENDER_FRONTEND_BASE_URL}/?extractId=${teaser.node.data.id}`)}`
+
+            const nodeWidth = 100 / teaser.nodes.length
+
+            return <div key={teaser.id} {...styles.item}>
+              <div style={{ position: 'relative' }} data-teaser={teaser.id}>
+                <img
+                  onLoad={this.measure}
+                  src={`${ASSETS_SERVER_BASE_URL}/render?width=1200&height=1&url=${encodeURIComponent(`${RENDER_FRONTEND_BASE_URL}/?extractId=${teaser.id}`)}&resize=160`}
+                  style={{
+                    width: '100%'
+                  }} />
+                {teaser.nodes.map((node, i) => {
+                  const area = (
+                    <a key={node.data.id} style={{
+                      display: 'block',
+                      position: 'absolute',
+                      left: `${nodeWidth * i}%`,
+                      width: `${nodeWidth}%`,
+                      top: 0,
+                      bottom: 0
+                    }} />
+                  )
+                  if (node.data.url) {
+                    return <HrefLink key={node.data.id} href={node.data.url} passHref>
+                      {area}
+                    </HrefLink>
+                  }
+                  return area
+                })}
+              </div>
+            </div>
+          })}
+        </LazyLoad>
+        {hover && <div style={{
+          position: 'absolute',
+          top: hover.measurement.y - (hoverTouch ? 40 : 20),
+          left: hover.measurement.x > 300 / 2
+            ? hover.measurement.x + 300 / 2 > width
+              ? width - 300
+              : hover.measurement.x + hover.measurement.width / 2 - 150
+            : 0
+        }}>
+          <img
+            style={{
+              width: 300,
+              position: 'absolute',
+              bottom: 0,
+              boxShadow: '0 2px 8px rgba(0,0,0,.4)'
+            }}
+            key={hover.teaser.id}
+            src={`${ASSETS_SERVER_BASE_URL}/render?width=1200&height=1&url=${encodeURIComponent(`${RENDER_FRONTEND_BASE_URL}/?extractId=${hover.teaser.id}`)}&resize=600`} />
+        </div>}
+        <div
+          {...styles.hoverArea}
+          onMouseEnter={this.focus}
+          onMouseMove={this.focus}
+          onMouseLeave={this.blur}
+          ref={
+            /* touch events are added via ref for { passive: false } on touchstart
+             * react does not support setting passive, which defaults to true in newer browsers
+             * https://github.com/facebook/react/issues/6436
+             */
+            this.hoverRef
+          } />
+      </div>
+    )
+  }
 }
 
 class FrontOverview extends Component {
@@ -159,56 +352,7 @@ class FrontOverview extends Component {
                   <P style={{ marginBottom: 20 }}>
                     {texts[year] && texts[year][month]}
                   </P>
-                  <LazyLoad visible={i === 0} attributes={{
-                    ...styles.container,
-                    ...css({
-                      ...SIZES.reduce((styles, size) => {
-                        const height = values.length / size.columns * 66
-                        if (size.minWidth) {
-                          styles[`@media only screen and (min-width: ${size.minWidth}px)`] = {
-                            height
-                          }
-                        } else {
-                          styles.height = height
-                        }
-                        return styles
-                      }, {})
-                    })
-                  }}>
-                    {values.map(teaser => {
-                      // `${ASSETS_SERVER_BASE_URL}/render?width=1200&height=1&url=${encodeURIComponent(`${RENDER_FRONTEND_BASE_URL}/?extractId=${teaser.node.data.id}`)}`
-
-                      const nodeWidth = 100 / teaser.nodes.length
-
-                      return <div key={teaser.id} {...styles.item}>
-                        <div style={{ position: 'relative' }}>
-                          <img
-                            src={`${ASSETS_SERVER_BASE_URL}/render?width=1200&height=1&url=${encodeURIComponent(`${RENDER_FRONTEND_BASE_URL}/?extractId=${teaser.id}`)}&resize=160`}
-                            style={{
-                              width: '100%'
-                            }} />
-                          {teaser.nodes.map((node, i) => {
-                            const area = (
-                              <a style={{
-                                display: 'block',
-                                position: 'absolute',
-                                left: `${nodeWidth * i}%`,
-                                width: `${nodeWidth}%`,
-                                top: 0,
-                                bottom: 0
-                              }} />
-                            )
-                            if (node.data.url) {
-                              return <HrefLink href={node.data.url} passHref>
-                                {area}
-                              </HrefLink>
-                            }
-                            return area
-                          })}
-                        </div>
-                      </div>
-                    })}
-                  </LazyLoad>
+                  <TeaserBlock teasers={values} lazy={i !== 0} />
                 </div>
               )
             })

db2478e002b625c54bb197efb4c5a4aa5d8cfb7b
Thomas Preusse <tpreusse@users.noreply.github.com>
2018-12-26 02:26:43 +0100

    optimise hover

--- components/Overview/TeaserHover.js
+++ components/Overview/TeaserHover.js
@@ -5,14 +5,17 @@ import { ASSETS_SERVER_BASE_URL, RENDER_FRONTEND_BASE_URL } from '../../lib/cons
 import TeaserNodes from './TeaserNodes'
 
 const TeaserHover = ({ measurement, teaser, width, highlight }) => {
-  const hoverWidth = 300
+  const hoverWidth = typeof window !== 'undefined' && window.innerWidth > 420
+    ? 400
+    : 300
+  const oneData = teaser.nodes.length === 1 && teaser.nodes[0].data
   return (
     <div style={{
       position: 'absolute',
       zIndex: 1,
       top: measurement.y - 10,
       left: measurement.x > hoverWidth / 2
-        ? measurement.x + hoverWidth / 2 > width
+        ? measurement.x + measurement.width / 2 + hoverWidth / 2 > width
           ? width - hoverWidth
           : measurement.x + measurement.width / 2 - hoverWidth / 2
         : 0
@@ -21,7 +24,7 @@ const TeaserHover = ({ measurement, teaser, width, highlight }) => {
         width: hoverWidth,
         position: 'absolute',
         bottom: 0,
-        backgroundColor: '#E5E5E5',
+        backgroundColor: (oneData && oneData.bgColor) || '#E5E5E5',
         minHeight: Math.floor(hoverWidth * measurement.height / measurement.width) - 2,
         lineHeight: 0,
         boxShadow: '0 2px 8px rgba(0,0,0,.4)'
@@ -32,7 +35,7 @@ const TeaserHover = ({ measurement, teaser, width, highlight }) => {
             width: '100%'
           }}
           key={teaser.id}
-          src={`${ASSETS_SERVER_BASE_URL}/render?width=1200&height=1&url=${encodeURIComponent(`${RENDER_FRONTEND_BASE_URL}/?extractId=${teaser.id}`)}&resize=600`} />
+          src={`${ASSETS_SERVER_BASE_URL}/render?width=1200&height=1&url=${encodeURIComponent(`${RENDER_FRONTEND_BASE_URL}/?extractId=${teaser.id}`)}&resize=800&format=jpeg`} />
         <TeaserNodes nodes={teaser.nodes} highlight={highlight} />
       </div>
     </div>

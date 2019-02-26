63e3b096a3fe36e5b59f2b455b253ad057533482
Daniel Pfänder <daniel.pfaender@project-r.construction>
2019-02-26 10:19:48 +0100

    fix(Progress): swissify date preposition

--- components/Article/Progress/datetime.test.js
+++ components/Article/Progress/datetime.test.js
@@ -19,13 +19,13 @@ import { t } from '../../../lib/withT'
     'two days ago',
     new Date(2019, 1, 13, 7, 14, 0),
     new Date(2019, 1, 15, 0, 0, 0),
-    'Ihre Leseposition vom 13.02.2019 um 07:14 Uhr.'
+    'Ihre Leseposition am 13.02.2019 um 07:14 Uhr.'
   ],
   [
     'one year ago',
     new Date(2018, 1, 15, 7, 14, 0),
     new Date(2019, 1, 15, 0, 0, 0),
-    'Ihre Leseposition vom 15.02.2018 um 07:14 Uhr.'
+    'Ihre Leseposition am 15.02.2018 um 07:14 Uhr.'
   ]
 ].map(([title, date, lastMidnight, expected]) => {
   test(`datetime.${title}`, assert => {

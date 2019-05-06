d4afdc8d9f8b4176df64de0bf5f6010dad81b7c3
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-05-07 00:00:57 +0200

    handle loading state with blur, transition and pulsating

--- components/Overview/TeaserHover.js
+++ components/Overview/TeaserHover.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { css } from 'glamor'
 
 import TeaserNodes from './TeaserNodes'
@@ -6,56 +6,86 @@ import { getSmallImgSrc } from './utils'
 
 import { ZINDEX_POPOVER } from '../constants'
 
-const TeaserHover = ({ measurement, teaser, contextWidth, highlight }) => {
-  const hoverWidth = typeof window !== 'undefined' && window.innerWidth > 420
-    ? 400
-    : 300
-  const oneData = teaser.nodes.length === 1 && teaser.nodes[0].data
-  return (
-    <div style={{
-      position: 'absolute',
-      zIndex: ZINDEX_POPOVER,
-      top: measurement.y - 10,
-      left: measurement.x > hoverWidth / 2
-        ? measurement.x + measurement.width / 2 + hoverWidth / 2 > contextWidth
-          ? contextWidth - hoverWidth
-          : measurement.x + measurement.width / 2 - hoverWidth / 2
-        : 0
-    }}>
+const styles = {
+  preview: css({
+    display: 'block',
+    position: 'absolute',
+    top: 0,
+    left: 0,
+    width: '100%',
+    filter: 'blur(5px)',
+    transform: 'scale(1)'
+  }),
+  iframe: css({
+    position: 'relative',
+    width: '100%',
+    height: '100%',
+    transition: 'opacity 200ms'
+  })
+}
+
+class TeaserHover extends Component {
+  constructor (props) {
+    super(props)
+
+    this.state = { loading: true }
+  }
+  render () {
+    const { measurement, teaser, contextWidth, highlight } = this.props
+    const { loading } = this.state
+
+    const hoverWidth = typeof window !== 'undefined' && window.innerWidth > 420
+      ? 400
+      : 300
+    const onLoadEnd = () => this.setState({ loading: false })
+    return (
       <div style={{
-        width: hoverWidth,
         position: 'absolute',
-        bottom: 0,
-        backgroundColor: (oneData && oneData.bgColor) || '#E5E5E5',
-        backgroundImage: `url(${getSmallImgSrc(teaser)})`,
-        backgroundSize: 'cover',
-        height: Math.floor(hoverWidth * measurement.height / measurement.width) - 2,
-        overflow: 'hidden',
-        lineHeight: 0,
-        boxShadow: '0 2px 8px rgba(0,0,0,.4)'
+        zIndex: ZINDEX_POPOVER,
+        top: measurement.y - 10,
+        left: measurement.x > hoverWidth / 2
+          ? measurement.x + measurement.width / 2 + hoverWidth / 2 > contextWidth
+            ? contextWidth - hoverWidth
+            : measurement.x + measurement.width / 2 - hoverWidth / 2
+          : 0
       }}>
-        <div {...css({
+        <div style={{
+          width: hoverWidth,
           position: 'absolute',
-          top: 0,
-          width: 1200,
-          height: Math.floor(1200 * measurement.height / measurement.width),
-          transform: `scale(${hoverWidth / 1200})`,
-          transformOrigin: '0% 0%'
-        })}>
-          <iframe
-            frameBorder='0'
-            sandbox=''
-            src={`/?extractId=${teaser.id}`}
-            style={{
-              position: 'relative',
-              width: '100%',
-              height: '100%'
-            }} />
+          bottom: 0,
+          height: Math.ceil(hoverWidth * measurement.height / measurement.width) - 2,
+          lineHeight: 0,
+          boxShadow: '0 2px 8px rgba(0,0,0,.4)'
+        }}>
+          <div {...css({
+            position: 'absolute',
+            top: 0,
+            width: 1200,
+            height: Math.floor(1200 * measurement.height / measurement.width),
+            overflow: 'hidden',
+            transform: `scale(${hoverWidth / 1200})`,
+            transformOrigin: '0% 0%'
+          })}>
+            <img {...styles.preview} src={getSmallImgSrc(teaser)} />
+            <iframe
+              frameBorder='0'
+              sandbox=''
+              onLoad={onLoadEnd}
+              onError={onLoadEnd}
+              src={`/?extractId=${teaser.id}`}
+              {...styles.iframe}
+              style={{
+                opacity: loading ? 0 : 1
+              }} />
+            <TeaserNodes
+              loading={loading}
+              nodes={teaser.nodes}
+              highlight={highlight} />
+          </div>
         </div>
-        <TeaserNodes nodes={teaser.nodes} highlight={highlight} />
       </div>
-    </div>
-  )
+    )
+  }
 }
 
 export default TeaserHover
--- components/Overview/TeaserNodes.js
+++ components/Overview/TeaserNodes.js
@@ -3,30 +3,46 @@ import { css } from 'glamor'
 
 import HrefLink from '../Link/Href'
 
+const loadingKeyframes = css.keyframes({
+  'from, to': {
+    opacity: 0.5
+  },
+  '50%': {
+    opacity: 1
+  }
+})
+
 const styles = {
   area: css({
     display: 'block',
     position: 'absolute',
     top: 0,
     bottom: 0,
-    transition: 'background-color 300ms'
+    transition: 'background-color 200ms, opacity 200ms'
+  }),
+  areaLoading: css({
+    animation: `2s ${loadingKeyframes} infinite ease-in-out`
   })
 }
 
-const TeaserNodes = ({ nodes, highlight, noClick }) => {
+const TeaserNodes = ({ nodes, loading, highlight, noClick }) => {
   const nodeWidth = 100 / nodes.length
   const maxIndex = nodes.length - 1
 
   return <Fragment>
     {nodes.map((node, i) => {
       const area = (
-        <a key={node.data.id} {...styles.area} style={{
-          left: `${nodeWidth * i}%`,
-          right: `${nodeWidth * (maxIndex - i)}%`,
-          backgroundColor: highlight && !highlight(node.data)
-            ? 'rgba(0,0,0,0.6)'
-            : 'rgba(0,0,0,0.0)'
-        }} />
+        <a
+          key={node.data.id}
+          {...styles.area}
+          {...loading && styles.areaLoading}
+          style={{
+            left: `${nodeWidth * i}%`,
+            right: `${nodeWidth * (maxIndex - i)}%`,
+            backgroundColor: loading || (highlight && !highlight(node.data))
+              ? 'rgba(0,0,0,0.6)'
+              : 'rgba(0,0,0,0.0)'
+          }} />
       )
       if (node.data.url && !noClick) {
         return <HrefLink key={node.data.id} href={node.data.url} passHref>

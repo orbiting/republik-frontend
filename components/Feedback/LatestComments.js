681d506fe5c80443876418b3b41837df05791f0b
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-06-13 15:40:40 +0200

    toggle between timeago and timeduration for latest comments

--- components/Feedback/LatestComments.js
+++ components/Feedback/LatestComments.js
@@ -1,13 +1,21 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { compose } from 'react-apollo'
 import { css } from 'glamor'
 import { withComments } from './enhancers'
 import withT from '../../lib/withT'
 import timeago from '../../lib/timeago'
+import timeduration from '../../lib/timeduration'
 
 import { GENERAL_FEEDBACK_DISCUSSION_ID } from '../../lib/constants'
 
-import { CommentTeaser, Loader, fontStyles, linkRule } from '@project-r/styleguide'
+import {
+  CommentTeaser,
+  Loader,
+  fontStyles,
+  linkRule,
+  mediaQueries,
+  useMediaQuery
+} from '@project-r/styleguide'
 
 import CommentLink from '../Discussion/CommentLink'
 
@@ -25,77 +33,79 @@ const styles = {
   })
 }
 
-class LatestComments extends Component {
-  render () {
-    const { t, data, fetchMore } = this.props
+const LatestComments = ({ t, data, fetchMore }) => {
+  const isDesktop = useMediaQuery(mediaQueries.mUp)
 
-    const timeagoFromNow = createdAtString => {
+  const timeagoFromNow = createdAtString => {
+    if (isDesktop) {
       return timeago(t, (new Date() - Date.parse(createdAtString)) / 1000)
+    } else {
+      return timeduration(t, (new Date() - Date.parse(createdAtString)) / 1000)
     }
-
-    return (
-      <Loader
-        loading={data.loading}
-        error={data.error}
-        render={() => {
-          const { comments } = data
-          const { pageInfo } = comments
-          return (
-            <div>
-              {comments && comments.nodes
-                .map(
-                  node => {
-                    const {
-                      id,
-                      discussion,
-                      preview,
-                      displayAuthor,
-                      published,
-                      createdAt,
-                      updatedAt,
-                      tags,
-                      parentIds
-                    } = node
-                    const meta = (discussion && discussion.document && discussion.document.meta) || {}
-                    const isGeneral = discussion.id === GENERAL_FEEDBACK_DISCUSSION_ID
-                    const newPage = !isGeneral && meta.template === 'discussion'
-
-                    return (
-                      <CommentTeaser
-                        key={id}
-                        id={id}
-                        t={t}
-                        displayAuthor={displayAuthor}
-                        preview={preview}
-                        published={published}
-                        createdAt={createdAt}
-                        updatedAt={updatedAt}
-                        timeago={timeagoFromNow}
-                        tags={tags}
-                        parentIds={parentIds}
-                        Link={CommentLink}
-                        discussion={discussion}
-                        newPage={newPage}
-                      />
-                    )
-                  }
-                )}
-              {pageInfo.hasNextPage && (
-                <button
-                  {...styles.button}
-                  {...linkRule}
-                  onClick={() => {
-                    fetchMore({ after: pageInfo.endCursor })
-                  }}
-                >
-                  {t('feedback/fetchMore')}
-                </button>
-              )}
-            </div>
-          )
-        }} />
-    )
   }
+
+  return (
+    <Loader
+      loading={data.loading}
+      error={data.error}
+      render={() => {
+        const { comments } = data
+        const { pageInfo } = comments
+        return (
+          <div>
+            {comments && comments.nodes
+              .map(
+                node => {
+                  const {
+                    id,
+                    discussion,
+                    preview,
+                    displayAuthor,
+                    published,
+                    createdAt,
+                    updatedAt,
+                    tags,
+                    parentIds
+                  } = node
+                  const meta = (discussion && discussion.document && discussion.document.meta) || {}
+                  const isGeneral = discussion.id === GENERAL_FEEDBACK_DISCUSSION_ID
+                  const newPage = !isGeneral && meta.template === 'discussion'
+
+                  return (
+                    <CommentTeaser
+                      key={id}
+                      id={id}
+                      t={t}
+                      displayAuthor={displayAuthor}
+                      preview={preview}
+                      published={published}
+                      createdAt={createdAt}
+                      updatedAt={updatedAt}
+                      timeago={timeagoFromNow}
+                      tags={tags}
+                      parentIds={parentIds}
+                      Link={CommentLink}
+                      discussion={discussion}
+                      newPage={newPage}
+                    />
+                  )
+                }
+              )}
+            {pageInfo.hasNextPage && (
+              <button
+                {...styles.button}
+                {...linkRule}
+                onClick={() => {
+                  fetchMore({ after: pageInfo.endCursor })
+                }}
+              >
+                {t('feedback/fetchMore')}
+              </button>
+            )}
+          </div>
+        )
+      }} />
+  )
 }
 
 export default compose(

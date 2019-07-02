cfe778b4041ef9f37abe4ce583febae152aae306
Daniel Pfänder <daniel.pfaender@project-r.construction>
2019-07-02 13:43:15 +0200

    minor translation changes and code DRY-up

--- components/Vote/201907/data.js
+++ components/Vote/201907/data.js
@@ -4,89 +4,91 @@ import { sum } from 'd3-array'
 import { colors } from '@project-r/styleguide'
 import { vt } from '../voteT'
 
+const t = key => vt(`vote/201907/budget/${key}`)
+
 // pk: Personalkosten
 // sk: Sachkosten
 
 const data = [
   {
-    category: vt('vote/201907/budget/redaktion'),
-    label: vt('vote/201907/budget/redaktion/0'),
+    category: t('redaktion'),
+    label: t('redaktion/0'),
     pk: 1788662,
     sk: 365000,
     background: colors.discrete[0],
-    more: vt('vote/201907/budget/redaktion/more')
+    more: t('redaktion/more')
   },
   {
-    category: vt('vote/201907/budget/redaktion'),
-    label: vt('vote/201907/budget/redaktion/1'),
+    category: t('redaktion'),
+    label: t('redaktion/1'),
     pk: 108885,
     sk: 4800
   },
   {
-    category: vt('vote/201907/budget/redaktion'),
-    label: vt('vote/201907/budget/redaktion/2'),
+    category: t('redaktion'),
+    label: t('redaktion/2'),
     pk: 0,
     sk: 120000
   },
   {
-    category: vt('vote/201907/budget/redaktion'),
-    label: vt('vote/201907/budget/redaktion/3'),
+    category: t('redaktion'),
+    label: t('redaktion/3'),
     pk: 174857,
     sk: 246800
   },
   {
-    category: vt('vote/201907/budget/redaktion'),
-    label: vt('vote/201907/budget/redaktion/4'),
+    category: t('redaktion'),
+    label: t('redaktion/4'),
     pk: 74291,
     sk: 82400
   },
   {
-    category: vt('vote/201907/budget/redaktion'),
-    label: vt('vote/201907/budget/redaktion/5'),
+    category: t('redaktion'),
+    label: t('redaktion/5'),
     pk: 382682,
     sk: 77600
   },
   {
-    category: vt('vote/201907/budget/it'),
-    label: vt('vote/201907/budget/it/0'),
+    category: t('it'),
+    label: t('it/0'),
     pk: 629903,
     sk: 185400,
     background: colors.discrete[1],
-    more: vt('vote/201907/budget/it/more')
+    more: t('it/more')
   },
   {
-    category: vt('vote/201907/budget/it'),
-    label: vt('vote/201907/budget/it/1'),
+    category: t('it'),
+    label: t('it/1'),
     pk: 0,
     sk: 40000
   },
   {
-    category: vt('vote/201907/budget/community'),
-    label: vt('vote/201907/budget/community'),
+    category: t('community'),
+    label: t('community'),
     pk: 422699,
     sk: 394000,
     background: colors.discrete[2],
-    more: vt('vote/201907/budget/community/more')
+    more: t('community/more')
   },
   {
-    category: vt('vote/201907/budget/services'),
-    label: vt('vote/201907/budget/services'),
+    category: t('services'),
+    label: t('services'),
     pk: 177692,
     sk: 248600,
     background: colors.discrete[3],
-    more: vt('vote/201907/budget/services/more')
+    more: t('services/more')
   },
   {
-    category: vt('vote/201907/budget/management'),
-    label: vt('vote/201907/budget/management/0'),
+    category: t('management'),
+    label: t('management/0'),
     pk: 146800,
     sk: 190300,
     background: colors.discrete[4],
-    more: vt('vote/201907/budget/management/more')
+    more: t('management/more')
   },
   {
-    category: vt('vote/201907/budget/management'),
-    label: vt('vote/201907/budget/management/1'),
+    category: t('management'),
+    label: t('management/1'),
     pk: 34300,
     sk: 88000
   }

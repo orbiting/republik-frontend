1fe61e78cb6c7119e911f3c1938c191b3b1cba38
Daniel Pfänder <daniel.pfaender@project-r.construction>
2019-07-16 15:12:45 +0200

    Marketing v2: 2 columns for cards on desktop (WIP)

--- components/Marketing/Cards.js
+++ components/Marketing/Cards.js
@@ -5,13 +5,15 @@ import { css } from 'glamor'
 import {
   Editorial,
   FigureImage,
-  mediaQueries
+  mediaQueries,
+  useMediaQuery
 } from '@project-r/styleguide'
 import { negativeColors } from '../Frame/constants'
 import { t } from '../../lib/withT'
 
 const ASSETS_URL = 'https://cdn.republik.space/s3/republik-assets/assets/marketing/'
 const MAX_WIDTH = 800
+const PADDING = 15
 
 // Inspired by https://codesandbox.io/s/j0y0vpz59
 
@@ -23,14 +25,15 @@ const styles = {
     width: '100%',
     // height: '100%',
     height: '100vh',
-    maxHeight: '600px',
+    maxHeight: '1000px',
     [mediaQueries.mUp]: {
-      maxHeight: '1000px'
+      maxHeight: '740px'
     },
     textAlign: 'center',
     '& > div': {
       position: 'absolute',
-      width: '100%',
+      // width: '100%',
+      width: '440px',
       height: '100%',
       willChange: 'transform',
       display: 'flex',
@@ -52,7 +55,6 @@ const styles = {
       [mediaQueries.mUp]: {
         maxHeight: '1000px'
       },
-      maxWidth: `${MAX_WIDTH / 2}px`, // hd
       willChange: 'transform',
       borderRadius: '5px',
       boxShadow: '0 12px 50px -10px rgba(0, 0, 0, 0.4), 0 10px 10px -10px rgba(0, 0, 0, 0.1)',
@@ -142,21 +144,37 @@ const cards = [
   }
 ]
 
-const to = i => ({
+const cardWidthDesktop = (innerWidth) => (Math.min(MAX_WIDTH, innerWidth)) / 2
+
+const toMobile = i => ({
   x: 0,
-  y: 30 + i * 25,
+  y: 30 + 50 * i,
   scale: 1,
-  // rot: -12 + i * 2,
-  rot: -12 + Math.random() * 24,
+  rot: -6 + Math.random() * 12,
   delay: i * 100
 })
+
+const toDesktop = (i, innerWidth, cardWidth) => {
+  return {
+    x: Math.floor(innerWidth / 2) + (i % 2 ? PADDING * 2 : -cardWidth + PADDING),
+    y: 30 + (i - i % 2) * 25,
+    scale: 1,
+    rot: -6 + Math.random() * 12,
+    delay: i * 100
+  }
+}
+
 const from = i => ({ x: 0, rot: 0, scale: 1.5, y: -1000 })
 const trans = (r, s) => ` rotateY(${r / 10}deg) rotateZ(${r}deg) scale(${s})`
 
 const Cards = () => {
+  if (!process.browser) return null
+  const isDesktop = useMediaQuery(mediaQueries.mUp)
+  const cardWidth = isDesktop && cardWidthDesktop(window.innerWidth)
+  const to = isDesktop ? toDesktop : toMobile
   const [gone] = useState(() => new Set())
   const [downIndex, setDownIndex] = useState(undefined)
-  const [props, set] = useSprings(cards.length, i => ({ ...to(i), from: from(i) }))
+  const [props, set] = useSprings(cards.length, i => ({ ...to(i, window.innerWidth, cardWidth), from: from(i) }))
   const bind = useGesture(({ args: [index], down, delta: [xDelta], distance, direction: [xDir], velocity }) => {
     const trigger = velocity > 0.01
     const dir = xDir < 0 ? -1 : 1
@@ -169,11 +187,11 @@ const Cards = () => {
       if (index !== i) return
       const isGone = gone.has(index)
       const x = isGone ? (200 + window.innerWidth) * dir : down ? xDelta : 0
-      const rot = xDelta / 100 + (isGone ? dir * 10 * velocity : 0)
+      const rot = -6 + Math.random() * 12
       const scale = down ? 1.1 : 1 // Active cards lift up a bit
       return { x, rot, scale, delay: undefined, config: { friction: 50, tension: down ? 800 : isGone ? 200 : 500 } }
     })
-    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i)), 600)
+    if (!down && gone.size === cards.length) setTimeout(() => gone.clear() || set(i => to(i, window.innerWidth, cardWidth)), 600)
   })
 
   return (
@@ -181,7 +199,8 @@ const Cards = () => {
       {props.map(({ x, y, rot, scale }, i) => (
         <animated.div key={i} style={{
           transform: interpolate([x, y], (x, y) => `translate3d(${x}px,${y}px,0)`),
-          zIndex: i === downIndex ? 1 : undefined
+          zIndex: i === downIndex ? 1 : undefined,
+          width: isDesktop ? `${cardWidth - PADDING * 2}px` : '100%'
         }}>
           <animated.div {...bind(i)} style={{ transform: interpolate([rot, scale], trans) }}>
             <Editorial.Subhead style={{ marginTop: 0 }}>{cards[i].title}</Editorial.Subhead>

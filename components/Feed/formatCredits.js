7949af69c9777dd5306b42e8e0328863bc1c04c4
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-09-05 11:12:05 +0200

    fix update date edge case

--- components/Feed/formatCredits.js
+++ components/Feed/formatCredits.js
@@ -1,7 +1,13 @@
 export default function formatCredits (credits) {
-  return credits.map(c => {
+  return credits.map((c, i) => {
     if (c.type === 'text') {
-      const value = c.value.replace(/(,\s|^)\d{1,2}\.\d{1,2}\.\d{4}\s?/g, '')
+      const value = c.value
+        .replace(
+          i === 0
+            ? /(,\s|^)\d{1,2}\.\d{1,2}\.\d{4},?\s?/g
+            : /(,\s|^)\d{1,2}\.\d{1,2}\.\d{4}\s?/g,
+          ''
+        )
       return {
         ...c,
         value

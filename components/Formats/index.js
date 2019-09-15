c7b48a3ae0cd8c6b5cea7e627432e8ea0c0f67da
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-09-15 13:53:07 +0200

    rm feed from formats page

--- components/Formats/index.js
+++ components/Formats/index.js
@@ -1,14 +1,10 @@
-import React, { Component } from 'react'
-import { compose } from 'react-apollo'
+import React from 'react'
 import { css } from 'glamor'
-import withT from '../../lib/withT'
 
 import GroupedFormats from './GroupedFormats'
-import Latest from './Latest'
 
 import {
   Center,
-  Interaction,
   mediaQueries
 } from '@project-r/styleguide'
 
@@ -19,28 +15,12 @@ const styles = {
       maxWidth: '695px',
       padding: '60px 0 120px'
     }
-  }),
-  latest: css({
-    margin: '60px 0 20px 0',
-    [mediaQueries.mUp]: {
-      margin: '100px 0 40px 0'
-    }
   })
 }
 
-class Formats extends Component {
-  render () {
-    const { t } = this.props
-    return (
-      <Center {...styles.container}>
-        <GroupedFormats />
-        <Interaction.H2 {...styles.latest}>
-          {t('formats/latest')}
-        </Interaction.H2>
-        <Latest />
-      </Center>
-    )
-  }
-}
-
-export default compose(withT)(Formats)
+const Formats = () => (
+  <Center {...styles.container}>
+    <GroupedFormats />
+  </Center>
+)
+export default Formats

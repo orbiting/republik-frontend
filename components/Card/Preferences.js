fdf81033c6336a4ffc357ae483524be4dd9c9f77
Thomas Preusse <tpreusse@users.noreply.github.com>
2019-09-22 00:16:36 +0200

    nice slider

--- components/Card/Preferences.js
+++ components/Card/Preferences.js
@@ -1,19 +1,53 @@
 import React, { Fragment } from 'react'
+import { css } from 'glamor'
 
 import withT from '../../lib/withT'
-import { Checkbox, Label, Editorial } from '@project-r/styleguide'
+import { Checkbox, Label, Editorial, mediaQueries } from '@project-r/styleguide'
 
 import createPersistedState from '../../lib/hooks/use-persisted-state'
 
 import { Paragraph } from './Shared'
 import Spider, { axes as spiderAxes } from './Spider'
+import Slider from './Slider'
 
 export const useCardPreferences = createPersistedState('republik-card-preferences')
 
-const nullSmartspider = [-1, -1, -1, -1, -1, -1, -1, -1]
+const styles = {
+  mySmartspider: css({
+    position: 'relative'
+  }),
+  mySmartspiderSpider: css({
+    [mediaQueries.mUp]: {
+      position: 'absolute',
+      right: 0,
+      top: 0
+    }
+  })
+}
+
+const inactiveValue = -1
+const nullSmartspider = [inactiveValue, inactiveValue, inactiveValue, inactiveValue, inactiveValue, inactiveValue, inactiveValue, inactiveValue]
 
 const Filters = ({ t }) => {
   const [preferences, setPreferences] = useCardPreferences({})
+
+  const setSlider = (newValue, i) => setPreferences(p => {
+    let newSmartspider = [
+      ...(p.mySmartspider || nullSmartspider).slice(0, i),
+      newValue,
+      ...(p.mySmartspider || nullSmartspider).slice(i + 1)
+    ]
+    if (newSmartspider.every(v => v === inactiveValue)) {
+      newSmartspider = undefined
+    }
+    return {
+      ...p,
+      mySmartspiderSort: p.mySmartspiderSort === undefined
+        ? newValue >= 0 ? true : undefined
+        : p.mySmartspiderSort,
+      mySmartspider: newSmartspider
+    }
+  })
 
   return <>
     <Paragraph>
@@ -59,53 +93,60 @@ const Filters = ({ t }) => {
     </Paragraph>
     <br />
 
-    {(preferences.mySmartspider || nullSmartspider).map((value, i) => {
-      return <Fragment key={i}>
-        <Label>{spiderAxes[i].text.replace(/\n/g, ' ')}</Label><br />
-        <input type='range'
-          value={value}
-          min='-10'
-          max='100'
-          onChange={(e) => {
-            const newValue = +e.target.value
-            setPreferences(p => ({ ...p,
-              mySmartspiderSort: p.mySmartspiderSort === undefined
-                ? true
-                : p.mySmartspiderSort,
-              mySmartspider: [
-                ...(p.mySmartspider || nullSmartspider).slice(0, i),
-                newValue,
-                ...(p.mySmartspider || nullSmartspider).slice(i + 1)
-              ] }))
-          }} />
-        <br />
-      </Fragment>
-    })}
-    {preferences.mySmartspider && <>
-      <br />
-      <Checkbox
-        checked={preferences.mySmartspiderSort}
-        onChange={(_, checked) => {
-          setPreferences(p => ({ ...p, mySmartspiderSort: checked }))
-        }}
-      >
-        {t('components/Card/Preferences/mySmartspider/sort')}
-      </Checkbox>
-      <br style={{ clear: 'left' }} />
-      <Spider
-        size={280}
-        fill={'#000'}
-        data={preferences.mySmartspider} />
-      <br /><br />
-      <Editorial.A href='#' onClick={(e) => {
-        e.preventDefault()
-        if (window.confirm(t('components/Card/Preferences/mySmartspider/resetConfirm'))) {
-          setPreferences(({ mySmartspider, ...rest }) => rest)
+    <div {...styles.mySmartspider}>
+      {(preferences.mySmartspider || nullSmartspider).map((value, i) => {
+        const onUp = (e) => {
+          if (e.target.value < 0) {
+            setSlider(inactiveValue, i)
+          }
         }
-      }}>
-        {t('components/Card/Preferences/mySmartspider/reset')}
-      </Editorial.A>
+        return <Fragment key={i}>
+          <Label>
+            {spiderAxes[i].text.replace(/\n/g, ' ').trim()}:
+            {' '}
+            {value < 0 ? 'keine Angaben' : value}
+          </Label><br />
+          <Slider
+            value={value}
+            min={inactiveValue}
+            max='100'
+            inactive={value < 0 || value === null}
+            onChange={(e, newValue) => {
+              setSlider(newValue, i)
+            }}
+            onMouseUp={onUp}
+            onTouchEnd={onUp} />
+          <br />
+        </Fragment>
+      })}
+      {preferences.mySmartspider && <>
+        <br />
+        <Checkbox
+          checked={preferences.mySmartspiderSort}
+          onChange={(_, checked) => {
+            setPreferences(p => ({ ...p, mySmartspiderSort: checked }))
+          }}
+        >
+          {t('components/Card/Preferences/mySmartspider/sort')}
+        </Checkbox>
+        <br style={{ clear: 'left' }} />
+        <div {...styles.mySmartspiderSpider}>
+          <Spider
+            size={280}
+            fill={'#000'}
+            data={preferences.mySmartspider.map(v => Math.max(0, v))} />
+        </div>
+        <br /><br />
+        <Editorial.A href='#' onClick={(e) => {
+          e.preventDefault()
+          if (window.confirm(t('components/Card/Preferences/mySmartspider/resetConfirm'))) {
+            setPreferences(({ mySmartspider, ...rest }) => rest)
+          }
+        }}>
+          {t('components/Card/Preferences/mySmartspider/reset')}
+        </Editorial.A>
     </>}
+    </div>
   </>
 }
 

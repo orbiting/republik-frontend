f2782fa6b5561665e4e4dd110e6d0aad17879449
Anna Traussnig <anna.traussnig@gmail.com>
2019-10-09 15:35:18 +0200

    Prevent label in slider to resize with the article

--- components/FontSize/Overlay.js
+++ components/FontSize/Overlay.js
@@ -3,7 +3,8 @@ import React from 'react'
 import {
   DEFAULT_FONT_SIZE, Overlay, OverlayBody,
   OverlayToolbar, OverlayToolbarConfirm,
-  Interaction, Slider
+  Interaction, Slider,
+  fontStyles, colors
 } from '@project-r/styleguide'
 
 import MdClose from 'react-icons/lib/md/close'
@@ -11,10 +12,14 @@ import withT from '../../lib/withT'
 import { compose } from 'react-apollo'
 
 import { useFontSize } from '../../lib/fontSize'
+import { css } from 'glamor'
 
 const FontSizeOverlay = ({ t, onClose }) => {
   const [fontSize, setFontSize] = useFontSize(DEFAULT_FONT_SIZE)
   const fontPercentage = Math.round(100 * fontSize / DEFAULT_FONT_SIZE)
+  const labelStyle = css({
+    ...fontStyles.sansSerifRegular14,
+    color: colors.secondary })
 
   return (
     <Overlay onClose={onClose} mUpStyle={{ maxWidth: 400, minHeight: 'none' }}>
@@ -29,8 +34,8 @@ const FontSizeOverlay = ({ t, onClose }) => {
       </OverlayToolbar>
       <OverlayBody>
         <div>
+          <label {...labelStyle}>{'Font size: ' + fontPercentage + '%'}</label>
           <Slider
-            label={'Font size: ' + fontPercentage + '%'}
             value={fontSize}
             min='8'
             max='48'

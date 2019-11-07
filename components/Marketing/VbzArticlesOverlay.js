b5a92a046017cb868dec0dd18eccfafb215d3f1a
Anna Traussnig <anna.traussnig@gmail.com>
2019-11-07 13:49:33 +0100

    Remove duplicate code

--- components/Marketing/VbzArticlesOverlay.js
+++ components/Marketing/VbzArticlesOverlay.js
@@ -13,52 +13,6 @@ import {
 import MdClose from 'react-icons/lib/md/close'
 import { css } from 'glamor'
 
-const articles = [
-  {
-    headline:
-      'Was China mit seinen 60 Milliarden in der Schweiz schon so alles gekauft hat.',
-    url: 'https://www.republik.ch/2019/09/16/das-china-dilemma'
-  },
-  {
-    headline:
-      'Schwirrt der Kopf bei Quantenphysik? Gut, dann gehen wir jetzt einen Schritt weiter.',
-    url: 'https://www.republik.ch/2019/09/07/die-physiker'
-  },
-  {
-    headline:
-      'Google-Software im Klassenzimmer: Totalüberwachung von Minderjährigen?',
-    url: 'https://www.republik.ch/2019/07/02/der-spion-im-schulzimmer'
-  },
-  {
-    headline: 'Grossbritannien: We have not even begun to fuck ourselves.',
-    url: 'https://www.republik.ch/2019/10/05/die-schlacht-um-england'
-  },
-  {
-    headline:
-      'Keine kommende Katastrophe wurde je so gründlich untersucht wie die Klimaerwärmung. Und keine wurde so gründlich ignoriert. Wieso?',
-    url: 'https://www.republik.ch/2019/08/24/die-grosse-ueberforderung'
-  },
-  {
-    headline:
-      'Neu im Nationalrat? Für wen lobbyieren Sie – und was ist Ihr Preis?',
-    url: 'https://www.republik.ch/2019/10/02/interessen-vertreten'
-  },
-  {
-    headline: 'Hat Facebook Sie schon radikalisiert?',
-    url:
-      'https://www.republik.ch/2019/10/03/die-infrastruktur-des-netzes-spielt-den-extremisten-in-die-haende'
-  },
-  {
-    headline:
-      'Deutschlands Autobauer haben die E-Mobilität lange ignoriert, jetzt kommt die Retourkutsche.',
-    url: 'https://www.republik.ch/2019/09/20/die-retourkutsche'
-  },
-  {
-    headline: 'Die Swisscom braucht dringend ein Näxt Big Sing.',
-    url: 'https://www.republik.ch/2019/08/15/se-naext-big-sing'
-  }
-]
-
 const styles = {
   articleContainer: css({
     [mediaQueries.mUp]: {

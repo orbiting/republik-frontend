c2e65c4a26d1924a8a548b4412f7151213aa8502
Anna Traussnig <anna.traussnig@gmail.com>
2019-11-07 13:14:28 +0100

    Set up primitive modal with article links

--- components/Marketing/VbzArticlesOverlay.js
+++ components/Marketing/VbzArticlesOverlay.js
@@ -12,51 +12,81 @@ import {
 import MdClose from 'react-icons/lib/md/close'
 import { css } from 'glamor'
 
-const styles = {
-  headline: css({
-    padding: '0 20px 20px'
-    // backgroundColor: '#2c2e35'
-  })
-}
+const articles = [
+  {
+    headline:
+      'Was China mit seinen 60 Milliarden in der Schweiz schon so alles gekauft hat.',
+    url: 'https://www.republik.ch/2019/09/16/das-china-dilemma'
+  },
+  {
+    headline:
+      'Schwirrt der Kopf bei Quantenphysik? Gut, dann gehen wir jetzt einen Schritt weiter.',
+    url: 'https://www.republik.ch/2019/09/07/die-physiker'
+  },
+  {
+    headline:
+      'Google-Software im Klassenzimmer: Totalüberwachung von Minderjährigen?',
+    url: 'https://www.republik.ch/2019/07/02/der-spion-im-schulzimmer'
+  },
+  {
+    headline: 'Grossbritannien: We have not even begun to fuck ourselves.',
+    url: 'https://www.republik.ch/2019/10/05/die-schlacht-um-england'
+  },
+  {
+    headline:
+      'Keine kommende Katastrophe wurde je so gründlich untersucht wie die Klimaerwärmung. Und keine wurde so gründlich ignoriert. Wieso?',
+    url: 'https://www.republik.ch/2019/08/24/die-grosse-ueberforderung'
+  },
+  {
+    headline:
+      'Neu im Nationalrat? Für wen lobbyieren Sie – und was ist Ihr Preis?',
+    url: 'https://www.republik.ch/2019/10/02/interessen-vertreten'
+  },
+  {
+    headline: 'Hat Facebook Sie schon radikalisiert?',
+    url:
+      'https://www.republik.ch/2019/10/03/die-infrastruktur-des-netzes-spielt-den-extremisten-in-die-haende'
+  },
+  {
+    headline:
+      'Deutschlands Autobauer haben die E-Mobilität lange ignoriert, jetzt kommt die Retourkutsche.',
+    url: 'https://www.republik.ch/2019/09/20/die-retourkutsche'
+  },
+  {
+    headline: 'Die Swisscom braucht dringend ein Näxt Big Sing.',
+    url: 'https://www.republik.ch/2019/08/15/se-naext-big-sing'
+  }
+]
 
 export default ({ onClose }) => {
   return (
-    <Overlay onClose={onClose} mUpStyle={{ maxWidth: 400, minHeight: 'none' }}>
-      <div style={{ backgroundColor: '#2c2e35' }}>
+    <Overlay onClose={onClose} mUpStyle={{ maxWidth: 800, minHeight: 'none' }}>
+      <OverlayToolbar>
+        <Interaction.Emphasis
+          style={{ padding: '15px 20px', fontSize: 16, color: colors.text }}
+        >
+          Die Republik durch 9 Artikel entdecken
+        </Interaction.Emphasis>
         <OverlayToolbarConfirm
           onClick={onClose}
-          label={<MdClose size={24} fill="#fff" />}
+          label={<MdClose size={24} fill='#000' />}
         />
-        <div {...styles.headline}>
-          <Editorial.Subhead style={{ margin: '0 auto 20px', color: 'white' }}>
-            « Was China mit seinen 60 Milliarden in der Schweiz schon so alles
-            gekauft gekauft hat. »
-          </Editorial.Subhead>
-          <Button white style={{ height: 48 }}>
-            Artikel lesen
-          </Button>
-        </div>
-        <div
-          style={{
-            backgroundColor: colors.negative.primaryBg,
-            padding: '10px 20px 20px 20px',
-            color: colors.negative.text
-          }}
-        >
-          <div {...css(styles.serifBold17)}>
-            Was China mit seinen 60 Milliarden in der Schweiz schon so alles
-            gekauft gekauft hat.
-          </div>
-          <Editorial.P>
-            Was China mit seinen 60 Milliarden in der Schweiz schon so alles
-            gekauft gekauft hat.
-          </Editorial.P>
-          <Editorial.P>
-            Was China mit seinen 60 Milliarden in der Schweiz schon so alles
-            gekauft gekauft hat.
-          </Editorial.P>
-        </div>
-      </div>
+      </OverlayToolbar>
+      <OverlayBody>
+        <Editorial.UL>
+          {articles.map((article, index) => {
+            return (
+              <Editorial.LI key={index}>
+                <Editorial.A href={article.url}>{article.headline}</Editorial.A>
+              </Editorial.LI>
+            )
+          })}
+        </Editorial.UL>
+        <Editorial.P>
+          We have great articles, read more about us{' '}
+          <Editorial.A href='#'>here</Editorial.A>.
+        </Editorial.P>
+      </OverlayBody>
     </Overlay>
   )
 }

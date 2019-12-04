beb013d8a69bd24284803636a797d435ffe1089f
Anna Traussnig <anna.traussnig@gmail.com>
2019-12-04 10:03:52 +0100

    Remove unused code

--- components/Search/index.js
+++ components/Search/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from 'react'
+import React from 'react'
 import { css } from 'glamor'
 
 import Form from './Form'
@@ -17,49 +17,11 @@ const styles = {
   })
 }
 
-class Search extends Component {
-  constructor(props, ...args) {
-    super(props, ...args)
-
-    this.state = {
-      isMobile: true,
-      allowFocus: true
-    }
-
-    this.refreshSearch = () => {
-      this.setState({
-        allowFocus: !this.state.isMobile
-      })
-    }
-
-    // useWindowSize (check in iOS that it works)
-    this.handleResize = () => {
-      const isMobile = window.innerWidth < mediaQueries.mBreakPoint
-      if (isMobile !== this.state.isMobile) {
-        this.setState({ isMobile })
-      }
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener('resize', this.handleResize)
-    this.handleResize()
-  }
-
-  componentWillUnmount() {
-    window.removeEventListener('resize', this.handleResize)
-  }
-
-  render() {
-    return (
-      <Center {...styles.container}>
-        <Form />
-        <Filters />
-        <Sort />
-        <Results />
-      </Center>
-    )
-  }
-}
-
-export default Search
+export default () => (
+  <Center {...styles.container}>
+    <Form />
+    <Filters />
+    <Sort />
+    <Results />
+  </Center>
+)

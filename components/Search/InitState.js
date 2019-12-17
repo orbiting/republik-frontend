6afedf242f40e9f6017847fa6bfe70ac3c4d4548
Anna Traussnig <anna.traussnig@gmail.com>
2019-12-17 12:04:36 +0100

    Link to results

--- components/Search/InitState.js
+++ components/Search/InitState.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import { compose } from 'react-apollo'
 import withT from '../../lib/withT'
-import { Interaction, linkRule } from '@project-r/styleguide'
+import { Interaction, linkRule, colors } from '@project-r/styleguide'
 import Link from 'next/link'
 
-export default compose(withT)(({ t, query, dataAggregations }) => {
+const ResultLink = ({ query, text }) => (
+  <Link
+    href={{
+      pathname: '/suche',
+      query: { q: query }
+    }}
+    passHref
+  >
+    <a {...linkRule}>{text}</a>
+  </Link>
+)
+
+const ResultCount = compose(withT)(({ t, query, dataAggregations }) => {
   const totalCount =
     dataAggregations.search && dataAggregations.search.totalCount
   const results = t.pluralize('search/pageInfo/total', {
@@ -12,23 +24,21 @@ export default compose(withT)(({ t, query, dataAggregations }) => {
   })
 
   return (
+    <Interaction.P>
+      {totalCount ? (
+        <ResultLink query={query} text={results} />
+      ) : (
+        <span style={{ color: colors.lightText }}>{results}</span>
+      )}
+    </Interaction.P>
+  )
+})
+
+export default compose(withT)(({ t, query, dataAggregations }) => {
+  return (
     <div>
-      {query && (
-        <Interaction.P>
-          {totalCount ? (
-            <Link
-              href={{
-                pathname: '/suche',
-                query: { q: query }
-              }}
-              passHref
-            >
-              <a {...linkRule}>{results}</a>
-            </Link>
-          ) : (
-            <span>{results}</span>
-          )}
-        </Interaction.P>
+      {query && query.length > 2 && (
+        <ResultCount query={query} dataAggregations={dataAggregations} />
       )}
     </div>
   )

2f89f74f773aa322fe9d7e033c819a990e9795d7
Andreas Moor <github@grmnd.ch>
2020-02-18 14:03:21 +0100

    bump styleguide and introduce HeaderHeightProvider

--- components/Frame/index.js
+++ components/Frame/index.js
@@ -6,14 +6,19 @@ import {
   fontFamilies,
   mediaQueries,
   colors,
-  ColorContext
+  ColorContext,
+  HeaderHeightProvider
 } from '@project-r/styleguide'
 import Meta from './Meta'
 import Header from './Header'
 import Footer from './Footer'
 import Box from './Box'
 import ProlongBox from './ProlongBox'
-import { HEADER_HEIGHT, HEADER_HEIGHT_MOBILE } from '../constants'
+import {
+  HEADER_HEIGHT,
+  HEADER_HEIGHT_MOBILE,
+  HEADER_HEIGHT_CONFIG
+} from '../constants'
 import { css } from 'glamor'
 import withMe from '../../lib/apollo/withMe'
 import withT from '../../lib/withT'
@@ -96,60 +101,64 @@ const Index = ({
   pullable,
   dark
 }) => (
-  <ColorContext.Provider value={dark && colors.negative}>
-    <div {...(footer || inNativeApp ? styles.bodyGrowerContainer : undefined)}>
-      {/* body growing only needed when rendering a footer */}
+  <HeaderHeightProvider config={HEADER_HEIGHT_CONFIG}>
+    <ColorContext.Provider value={dark && colors.negative}>
       <div
-        {...(footer || inNativeApp ? styles.bodyGrower : undefined)}
-        {...(!cover ? styles.padHeader : undefined)}
+        {...(footer || inNativeApp ? styles.bodyGrowerContainer : undefined)}
       >
-        {dark && (
-          <style
-            dangerouslySetInnerHTML={{
-              __html: `html, body { background-color: ${colors.negative.containerBg}; color: ${colors.negative.text}; }`
-            }}
-          />
-        )}
-        {!!meta && <Meta data={meta} />}
-        <Header
-          dark={dark && !inNativeIOSApp}
-          me={me}
-          cover={cover}
-          onPrimaryNavExpandedChange={onPrimaryNavExpandedChange}
-          primaryNavExpanded={primaryNavExpanded}
-          secondaryNav={secondaryNav}
-          showSecondary={showSecondary}
-          formatColor={formatColor}
-          headerAudioPlayer={headerAudioPlayer}
-          pullable={pullable}
-        />
-        <noscript>
-          <Box style={{ padding: 30 }}>
-            <RawHtml
+        {/* body growing only needed when rendering a footer */}
+        <div
+          {...(footer || inNativeApp ? styles.bodyGrower : undefined)}
+          {...(!cover ? styles.padHeader : undefined)}
+        >
+          {dark && (
+            <style
               dangerouslySetInnerHTML={{
-                __html: t('noscript')
+                __html: `html, body { background-color: ${colors.negative.containerBg}; color: ${colors.negative.text}; }`
               }}
             />
-          </Box>
-        </noscript>
-        {me && me.prolongBeforeDate !== null && (
-          <ProlongBox
-            t={t}
-            prolongBeforeDate={me.prolongBeforeDate}
-            dark={dark}
+          )}
+          {!!meta && <Meta data={meta} />}
+          <Header
+            dark={dark && !inNativeIOSApp}
+            me={me}
+            cover={cover}
+            onPrimaryNavExpandedChange={onPrimaryNavExpandedChange}
+            primaryNavExpanded={primaryNavExpanded}
+            secondaryNav={secondaryNav}
+            showSecondary={showSecondary}
+            formatColor={formatColor}
+            headerAudioPlayer={headerAudioPlayer}
+            pullable={pullable}
           />
-        )}
-        {raw ? (
-          children
-        ) : (
-          <MainContainer>
-            <Content>{children}</Content>
-          </MainContainer>
-        )}
+          <noscript>
+            <Box style={{ padding: 30 }}>
+              <RawHtml
+                dangerouslySetInnerHTML={{
+                  __html: t('noscript')
+                }}
+              />
+            </Box>
+          </noscript>
+          {me && me.prolongBeforeDate !== null && (
+            <ProlongBox
+              t={t}
+              prolongBeforeDate={me.prolongBeforeDate}
+              dark={dark}
+            />
+          )}
+          {raw ? (
+            children
+          ) : (
+            <MainContainer>
+              <Content>{children}</Content>
+            </MainContainer>
+          )}
+        </div>
+        {!inNativeApp && footer && <Footer />}
       </div>
-      {!inNativeApp && footer && <Footer />}
-    </div>
-  </ColorContext.Provider>
+    </ColorContext.Provider>
+  </HeaderHeightProvider>
 )
 
 export default compose(

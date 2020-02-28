000582862b010f2db97be19c68bfb99d590756bd
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-02-28 18:56:27 +0100

    with subtitles

--- pages/crowdfunding2.js
+++ pages/crowdfunding2.js
@@ -145,6 +145,7 @@ const VIDEOS = {
       'https://player.vimeo.com/external/394299161.m3u8?s=04b073df4a9a2e46dbf3bb030a81d7b233b70e10',
     mp4:
       'https://player.vimeo.com/external/394299161.hd.mp4?s=52bbb16e068387bd4e44683de01cbfebdcbc95e1&profile_id=175',
+    subtitles: '/static/subtitles/cf2.vtt',
     thumbnail: `${CDN_FRONTEND_BASE_URL}/static/video/manifest.png`
   }
 }

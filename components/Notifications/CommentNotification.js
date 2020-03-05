91eac2e6426dd465cae880b76e9a85545b741752
Anna Traussnig <anna.traussnig@gmail.com>
2020-03-05 18:18:43 +0100

    Prototype susbcription UI

--- components/Notifications/CommentNotification.js
+++ components/Notifications/CommentNotification.js
@@ -1,13 +1,56 @@
 import React from 'react'
-import { CommentTeaser } from '@project-r/styleguide'
+import {
+  Checkbox,
+  CommentTeaser,
+  fontStyles,
+  mediaQueries
+} from '@project-r/styleguide'
 import { compose } from 'react-apollo'
 import withT from '../../lib/withT'
 import CommentLink from '../Discussion/CommentLink'
-import { merge } from 'glamor'
+import { css, merge } from 'glamor'
 import { isNewStyle } from './index'
 import NotificationChannelsLink from './NotificationChannelsLink'
 
-const UnsubscribeCallout = ({ me }) => <NotificationChannelsLink me={me} />
+const styles = {
+  checkbox: css({
+    '& label': {
+      display: 'flex',
+      textAlign: 'left',
+      alignItems: 'center',
+      margin: '5px 0',
+      [mediaQueries.mUp]: {
+        ...fontStyles.sansSerifRegular12
+      }
+    },
+    '& svg': {
+      width: 12,
+      height: 12
+    }
+  }),
+  checkboxLabel: css({
+    marginTop: -6
+  })
+}
+
+const UnsubscribeCallout = ({ me }) => (
+  <>
+    <div {...styles.checkbox}>
+      <label style={{ marginBottom: 10 }}>
+        <b>Subscribed via:</b>
+      </label>
+      <Checkbox checked={true} onChange={() => undefined}>
+        <span {...styles.checkboxLabel}>Debate</span>
+      </Checkbox>
+      <Checkbox checked={true} onChange={() => undefined}>
+        <span {...styles.checkboxLabel}>Profile</span>
+      </Checkbox>
+    </div>
+    <div style={{ marginTop: -10 }}>
+      <NotificationChannelsLink me={me} />
+    </div>
+  </>
+)
 
 export default compose(withT)(({ t, node, me, isNew }) => {
   return (

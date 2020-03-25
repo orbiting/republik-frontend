7d2a1df06ba73aaa5862a1375dd8135647882a24
Anna Traussnig <anna.traussnig@gmail.com>
2020-03-25 15:28:50 +0100

    Add icon next to format title

--- components/Notifications/SubscribeDocument.js
+++ components/Notifications/SubscribeDocument.js
@@ -1,11 +1,31 @@
 import React from 'react'
 import SubscribeDocumentCheckbox from './SubscribeDocumentCheckbox'
 import withT from '../../lib/withT'
+import Info from 'react-icons/lib/fa/info'
+import { A } from '@project-r/styleguide'
+import { css } from 'glamor'
+
+const styles = {
+  title: css({
+    whiteSpace: 'nowrap'
+  }),
+  icon: css({
+    fontSize: 12,
+    marginLeft: 5,
+    lineHeight: '20px',
+    verticalAlign: 'top'
+  })
+}
 
 const SubscribeDocument = ({ t, subscription, setAnimate, style }) => {
   return (
     <div style={style}>
-      <h4>{t('SubscribeDocument/title')}</h4>
+      <h4 {...styles.title}>
+        {t('SubscribeDocument/title')}
+        <A href='#' {...styles.icon}>
+          <Info />
+        </A>
+      </h4>
       <SubscribeDocumentCheckbox
         subscription={subscription}
         setAnimate={setAnimate}

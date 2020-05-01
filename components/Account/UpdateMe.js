94f30d01c3a1dbe1705c5a380135cead2b9405c9
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-05-01 15:46:21 +0200

    add explanation to address form name

--- components/Account/UpdateMe.js
+++ components/Account/UpdateMe.js
@@ -235,7 +235,13 @@ class UpdateMe extends Component {
                     }}
                     fields={meFields}
                   />
-                  <Label style={{ marginTop: -8, display: 'block' }}>
+                  <Label
+                    style={{
+                      marginTop: -8,
+                      display: 'block',
+                      color: colors.disabled
+                    }}
+                  >
                     {t('Account/Update/birthday/hint/plain')}
                   </Label>
                   <br />
@@ -356,8 +362,4 @@ class UpdateMe extends Component {
   }
 }
 
-export default compose(
-  withMyDetails,
-  withMyDetailsMutation,
-  withT
-)(UpdateMe)
+export default compose(withMyDetails, withMyDetailsMutation, withT)(UpdateMe)

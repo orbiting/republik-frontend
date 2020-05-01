185bf68df07b7c20d8715f755f876a9d2ebb4c28
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-05-01 12:41:34 +0200

    add max for cockpit 19 surpluss

--- components/Crowdfunding/withSurviveStatus.js
+++ components/Crowdfunding/withSurviveStatus.js
@@ -24,7 +24,7 @@ const statusQuery = gql`
       }
     }
     revenueStats {
-      surplus(min: "2019-11-30T23:00:00Z") {
+      surplus(min: "2019-11-30T23:00:00Z", max: "${END_DATE}") {
         total
         updatedAt
       }

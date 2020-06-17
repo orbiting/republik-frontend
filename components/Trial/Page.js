b59bcf6349695cd61ea76324bebdd9056841eb1e
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-06-17 15:32:14 +0200

    support formatting & image on trial page and custom button text in the form

--- components/Trial/Page.js
+++ components/Trial/Page.js
@@ -1,6 +1,7 @@
 import React, { Fragment } from 'react'
 import { compose } from 'react-apollo'
 import { withRouter } from 'next/router'
+import { css } from 'glamor'
 
 import Form from './Form'
 import withTrialEligibility from './withTrialEligibility'
@@ -13,35 +14,45 @@ import withMe from '../../lib/apollo/withMe'
 import withT from '../../lib/withT'
 import withInNativeApp from '../../lib/withInNativeApp'
 
-import { Button, Label, Interaction, RawHtml } from '@project-r/styleguide'
+import {
+  Button,
+  Label,
+  Interaction,
+  RawHtml,
+  mediaQueries
+} from '@project-r/styleguide'
 
 const { H1, P } = Interaction
 
 const trailCampaignes = parseJSONObject(TRIAL_CAMPAIGNS)
 const dayFormat = timeFormat('%e. %B %Y')
 
-const getTranslationKey = (name, { isAuthorized, hasAccess, campaign }) => {
-  if (!isAuthorized) {
-    return [
-      `Trial/Page/unauthorized/${campaign}/${name}`,
-      `Trial/Page/unauthorized/${name}`
-    ]
-  }
+const styles = {
+  image: css({
+    float: 'right',
+    maxWidth: 150,
+    maxHeight: 170,
+    paddingLeft: 10,
+    [mediaQueries.mUp]: {
+      paddingLeft: 30
+    }
+  })
+}
 
-  if (!hasAccess) {
-    return [
-      `Trial/Page/authorized/withoutAccess/${campaign}/${name}`,
-      `Trial/Page/authorized/withoutAccess/${name}`
-    ]
+const getTranslationKeys = (name, { isSignedIn, hasAccess, campaign }) => {
+  if (hasAccess) {
+    return [`Trial/Page/withAccess/${name}`]
   }
 
   return [
-    `Trial/Page/authorized/withAccess/${campaign}/${name}`,
-    `Trial/Page/authorized/withAccess/${name}`
-  ]
+    campaign && isSignedIn && `Trial/Page/${campaign}/${name}/signedIn`,
+    campaign && `Trial/Page/${campaign}/${name}`,
+    isSignedIn && `Trial/Page/${name}/signedIn`,
+    `Trial/Page/${name}`
+  ].filter(Boolean)
 }
 
-const ALLOWED_CAMPAIGNS = ['covid-19-uhr-newsletter', 'briefings']
+const ALLOWED_CAMPAIGNS = ['covid-19-uhr-newsletter', 'briefings', 'am-gericht']
 
 const Page = props => {
   const {
@@ -56,7 +67,7 @@ const Page = props => {
   const campaign = query.campaign || query.utm_campaign
 
   const until = viaActiveMembership.until || viaAccessGrant.until
-  const isAuthorized = !!me
+  const isSignedIn = !!me
   const hasAccess = !!until
 
   const accessCampaignId =
@@ -68,9 +79,9 @@ const Page = props => {
     return (
       <>
         <H1 style={{ marginBottom: 40 }}>{t('Trial/Page/disabled/title')}</H1>
-
         {t('Trial/Page/disabled/body')
           .split('\n\n')
+          .filter(Boolean)
           .map((c, i) => (
             <P key={i} style={{ marginTop: 30, marginBottom: 30 }}>
               <RawHtml
@@ -92,23 +103,43 @@ const Page = props => {
     )
   }
 
+  const content = t.first(
+    getTranslationKeys('intro', { isSignedIn, hasAccess, campaign }),
+    {
+      email: me && me.email,
+      until: until && dayFormat(new Date(until))
+    }
+  )
+  const image = t.first(
+    getTranslationKeys('image', { isSignedIn, hasAccess, campaign }),
+    {},
+    ''
+  )
   return (
     <Fragment>
-      <H1>
+      <H1 style={{ marginBottom: 20 }}>
         {t.first(
-          getTranslationKey('heading', { isAuthorized, hasAccess, campaign })
+          getTranslationKeys('heading', { isSignedIn, hasAccess, campaign })
         )}
       </H1>
-      <P style={{ marginTop: 40, marginBottom: 40 }}>
-        {t.first(
-          getTranslationKey('intro', { isAuthorized, hasAccess, campaign }),
-          {
-            email: me && me.email,
-            until: until && dayFormat(new Date(until))
-          }
-        )}
-      </P>
-      <Form initialEmail={initialEmail} accessCampaignId={accessCampaignId} />
+      {!!image && <img {...styles.image} src={image} />}
+      {content
+        .split('\n\n')
+        .filter(Boolean)
+        .map((c, i) => (
+          <P key={i} style={{ marginTop: 30, marginBottom: 30 }}>
+            <RawHtml
+              dangerouslySetInnerHTML={{
+                __html: c
+              }}
+            />
+          </P>
+        ))}
+      <Form
+        initialEmail={initialEmail}
+        accessCampaignId={accessCampaignId}
+        campaign={campaign}
+      />
     </Fragment>
   )
 }

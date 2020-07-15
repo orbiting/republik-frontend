20222cc9ce894110e207f825195534cf734a67c9
Thomas Preusse <tpreusse@users.noreply.github.com>
2020-07-15 11:34:18 +0200

    update overview texts

--- components/Overview/2020.js
+++ components/Overview/2020.js
@@ -77,5 +77,122 @@ export default {
       </Highlight>
       .
     </>
+  ),
+  März: p => (
+    <>
+      Was ist zu tun im Fall von{' '}
+      <Highlight {...p} ids={['QR9AH3qG3']}>
+        Panik wegen Corona?
+      </Highlight>{' '}
+      Ein Interview über{' '}
+      <Highlight {...p} ids={['JBenPEw7m']}>
+        Krisenmodus vs. Freiheitsrechte
+      </Highlight>{' '}
+      mit Staatsrechtler Daniel Moeckli. Teil 9 des{' '}
+      <Highlight
+        {...p}
+        series='republik/article-wir-machen-den-demokratie-check'
+      >
+        Demokratie-Checks
+      </Highlight>{' '}
+      mit Alt-Bundesrätin Ruth Dreifuss. Warum die{' '}
+      <Highlight {...p} ids={['GBpBQKj83', 'Ncq7t9yA3']}>
+        Wiederwahlkampagne von US-Präsident
+      </Highlight>{' '}
+      Donald Trump alles bisher Dagewesene in den Schatten stellt. Und der{' '}
+      <Highlight {...p} ids={['6aKEWKigS']}>
+        Nachruf auf unsere Bildchefin Brigitte Meyer
+      </Highlight>
+      .
+    </>
+  ),
+  April: p => (
+    <>
+      <Highlight {...p} series='republik/article-master-indien'>
+        Auftakt der Reportageserie über die hinduistisch-nationalistische
+        Regierung
+      </Highlight>{' '}
+      um Premier Narendra Modi, eine Reise an die{' '}
+      <Highlight {...p} ids={['JBenPEw7m']}>
+        Ränder der Gesellschaft
+      </Highlight>{' '}
+      zu Zeiten des Corona-Lockdown, die{' '}
+      <Highlight {...p} series='republik/article-kolumne-test-haerdoepfel'>
+        Corona-Rezept-Serie
+      </Highlight>{' '}
+      für Heimgastronomen, eine dreiteilige Reportage über weggewiesene{' '}
+      <Highlight {...p} series='republik/article-eritrea-auftakt'>
+        Asylsuchende aus Eritrea
+      </Highlight>
+      ,{' '}
+      <Highlight {...p} series='republik/article-brief-von-adolf-muschg'>
+        Briefwechsel aus der Quarantäne
+      </Highlight>
+      , ein Treffen mit{' '}
+      <Highlight {...p} ids={['i9-Izu1EO']}>
+        NZZ-Chefredaktor Eric Gujer
+      </Highlight>{' '}
+      und ein Interview mit Philosophin und Geschlechter­forscherin Patricia
+      Purtschert über den{' '}
+      <Highlight {...p} ids={['KMTxbUu6R']}>
+        Zusammenhang zwischen Geschlecht und Corona-Krise
+      </Highlight>
+      .
+    </>
+  ),
+  Mai: p => (
+    <>
+      Die{' '}
+      <Highlight {...p} series='republik/article-kmu-serie'>
+        Serie über Schweizer Unternehmen nach der Corona-Krise
+      </Highlight>
+      . Wie es zum Amtsenthebungsverfahren gegen Bundesanwalt Michael Lauber
+      kam. Über die{' '}
+      <Highlight {...p} ids={['bcTYcatVn']}>
+        Musik von morgen
+      </Highlight>{' '}
+      bzw. nach Corona und die{' '}
+      <Highlight {...p} series='republik/article-deutschland-einfuehrung'>
+        Reportageserie über den Anstieg rechtsextremen Terrors in Deutschland
+      </Highlight>
+      . Wie Feministinnen sich die{' '}
+      <Highlight {...p} ids={['2z4v5RLCZ']}>
+        Deutungshoheit über den weiblichen Körper zurückerobern
+      </Highlight>{' '}
+      und ein Interview mit{' '}
+      <Highlight {...p} ids={['1xfm1QP6L']}>
+        Medienministerin Simonetta Sommaruga
+      </Highlight>
+      .
+    </>
+  ),
+  Juni: p => (
+    <>
+      Über die{' '}
+      <Highlight {...p} ids={['u6gPQgw3Q']}>
+        Unschuld des Donald Trump
+      </Highlight>
+      , einen vom{' '}
+      <Highlight {...p} ids={['LIoK5zK63']}>
+        Nachrichtendienst konstruierten islamistischen Gefährder
+      </Highlight>
+      ,{' '}
+      <Highlight {...p} series='republik/article-kmu-serie'>
+        Wirtschaftspolitik in der Coronakrise
+      </Highlight>
+      , ein «
+      <Highlight {...p} ids={['LutNt7pxH']}>
+        Black Coming Out
+      </Highlight>
+      », die{' '}
+      <Highlight {...p} ids={['DO5ge3f2V', 'gcZCIxupB']}>
+        Retraumatisierung
+      </Highlight>{' '}
+      von Opfern sexueller Gewalt durch die Polizei, sowie{' '}
+      <Highlight {...p} ids={['3RaedFB5f']}>
+        Her Kindness Jacinda Ardern
+      </Highlight>
+      .
+    </>
   )
 }

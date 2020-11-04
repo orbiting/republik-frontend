9390859327bcf7c15b370c940120a45386392009
Olivier Baumann <olivier@iovi.io>
2020-11-04 08:52:14 +0100

    fixes backbutton color

--- components/Icons/Back.js
+++ components/Icons/Back.js
@@ -1,11 +1,12 @@
 import React from 'react'
 
-const Icon = ({ size = 24, fill, style }) => (
+const Icon = ({ size = 24, fill, style, ...props }) => (
   <svg
     width={size}
     height={size}
     viewBox='0 0 24 24'
     style={{ verticalAlign: 'middle', ...style }}
+    {...props}
   >
     <path d='M11.67 3.87L9.9 2.1 0 12l9.9 9.9 1.77-1.77L3.54 12z' fill={fill} />
     <path fill='none' d='M0 0h24v24H0z' />

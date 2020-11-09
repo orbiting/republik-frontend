f2cfbf68d5f2b95a3eff539bde5ade59a8eca159
Olivier Baumann <olivier@iovi.io>
2020-11-09 14:50:40 +0100

    wip: marketing

---
+++ components/Marketing/Sections/Dialogue.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { css } from 'glamor'
+import { Container } from '@project-r/styleguide'
+
+export default function DialogSection({ t }) {
+  return <Container {...styles.container}></Container>
+}
+
+const styles = {
+  container: css({})
+}

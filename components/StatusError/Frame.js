3a5036675c826a51039faf08a66f89998965be0b
Olivier Baumann <olivier@iovi.io>
2020-11-19 14:40:04 +0100

    error

--- components/StatusError/Frame.js
+++ components/StatusError/Frame.js
@@ -1,6 +1,6 @@
 // Derived from https://github.com/facebook/immutable-js/blob/7f4e61601d92fc874c99ccf7734d6f33239cec8c/pages/src/src/Header.js#L2
 
-import React, { Fragment, Component } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import { css } from 'glamor'
 import { range } from 'd3-array'
 
@@ -10,7 +10,7 @@ import {
   NarrowContainer,
   fontFamilies,
   mediaQueries,
-  colors
+  useColorContext
 } from '@project-r/styleguide'
 
 const styles = {
@@ -19,7 +19,6 @@ const styles = {
     left: 0,
     right: 0,
     fontFamily: fontFamilies.sansSerifMedium,
-    backgroundColor: colors.primaryBg,
     top: HEADER_HEIGHT_MOBILE,
     height: 220,
     paddingTop: 20,
@@ -36,11 +35,7 @@ const styles = {
     right: 0,
     textAlign: 'center'
   }),
-  secondary: css({
-    color: colors.primary
-  }),
   content: css({
-    backgroundColor: '#fff',
     position: 'relative',
     zIndex: 1,
     marginTop: 200,
@@ -52,74 +47,71 @@ const styles = {
   })
 }
 
-class Header extends Component {
-  constructor(...args) {
-    super(...args)
-    this.state = {
-      scroll: 0,
-      isMobile: true
-    }
+const ErrorFrame = ({ children, statusCode }) => {
+  const [colorScheme] = useColorContext()
+  const [isMobile, setIsMobile] = useState(true)
+  const [scroll, setScroll] = useState(0)
 
-    this.handleResize = () => {
-      const isMobile = window.innerWidth < mediaQueries.mBreakPoint
-      if (isMobile !== this.state.isMobile) {
-        this.setState({ isMobile })
+  const requestRef = useRef()
+
+  useEffect(() => {
+    const handleResize = () => {
+      const mobile = window.innerWidth < mediaQueries.mBreakPoint
+      if (mobile !== isMobile) {
+        setIsMobile(mobile)
       }
     }
-    this.handleScroll = () => {
-      if (!this._req) {
+
+    const handleScroll = () => {
+      if (!requestRef.current) {
         const headerHeight = window.innerHeight * 0.6
         if (window.scrollY < headerHeight) {
-          this._req = window.requestAnimationFrame(() => {
-            this._req = undefined
-            this.setState({ scroll: window.scrollY })
+          requestRef.current = window.requestAnimationFrame(() => {
+            requestRef.current = undefined
+            setScroll(Math.max(window.scrollY, 0))
           })
         }
       }
     }
-  }
-  componentDidMount() {
-    window.addEventListener('scroll', this.handleScroll)
-    window.addEventListener('resize', this.handleResize)
-    this.handleScroll()
-    this.handleResize()
-  }
-  componentWillUnmount() {
-    window.cancelAnimationFrame(this._req)
-    window.removeEventListener('scroll', this.handleScroll)
-    window.removeEventListener('resize', this.handleResize)
-  }
-  render() {
-    const { children, statusCode } = this.props
-    const { isMobile } = this.state
-    const scroll = Math.max(this.state.scroll, 0)
-    const space = isMobile ? 35 : 70
+    handleScroll()
+    handleResize()
+    window.addEventListener('scroll', handleScroll)
+    window.addEventListener('resize', handleResize)
 
-    return (
-      <Fragment>
-        <div {...styles.cover}>
-          {(isMobile ? range(7) : range(11)).map((_, i) => (
-            <div
-              key={i}
-              {...styles.headline}
-              {...styles.secondary}
-              style={t(y(scroll, i * space), z(scroll, i * space))}
-            >
-              <span style={{ opacity: o(scroll, i * space) }}>
-                {statusCode}
-              </span>
-            </div>
-          ))}
-          <div {...styles.headline} style={t(scroll * -0.55, 1)}>
-            <span>{statusCode}</span>
+    return () => {
+      window.cancelAnimationFrame(requestRef.current)
+      window.removeEventListener('scroll', handleScroll)
+      window.removeEventListener('resize', handleResize)
+    }
+  }, [])
+
+  const space = isMobile ? 35 : 70
+
+  return (
+    <>
+      <div {...styles.cover}>
+        {(isMobile ? range(7) : range(11)).map((_, i) => (
+          <div
+            key={i}
+            {...styles.headline}
+            {...colorScheme.set('color', 'primary')}
+            style={t(y(scroll, i * space), z(scroll, i * space))}
+          >
+            <span style={{ opacity: o(scroll, i * space) }}>{statusCode}</span>
           </div>
+        ))}
+        <div {...styles.headline} style={t(scroll * -0.55, 1)}>
+          <span>{statusCode}</span>
         </div>
-        <div {...styles.content}>
-          <NarrowContainer>{children}</NarrowContainer>
-        </div>
-      </Fragment>
-    )
-  }
+      </div>
+      <div
+        {...styles.content}
+        {...colorScheme.set('backgroundColor', 'default')}
+      >
+        <NarrowContainer>{children}</NarrowContainer>
+      </div>
+    </>
+  )
 }
 
 const y = (scroll, pos) => {
@@ -145,4 +137,4 @@ const t = (y, z) => {
   }
 }
 
-export default Header
+export default ErrorFrame

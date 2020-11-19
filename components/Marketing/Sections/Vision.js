320db821ccc108ba80775d029700c244e4051614
Olivier Baumann <olivier@iovi.io>
2020-11-19 13:57:30 +0100

    vision

---
+++ components/Marketing/Sections/Vision.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import { css } from 'glamor'
+
+import { Editorial, Center, Breakout } from '@project-r/styleguide'
+
+export const Vision = () => {
+  return (
+    <>
+      <Editorial.H2>
+        Ohne Journalismus keine Demokratie. Und ohne Demokratie keine Freiheit.
+      </Editorial.H2>
+    </>
+  )
+}
+
+const styles = {
+  test: css({})
+}
+
+export default Vision

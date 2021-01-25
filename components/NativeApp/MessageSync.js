5be9e886c5abd5c6e898f4fd56eeca688dfa2088
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-01-25 01:03:04 +0100

    refactor and cleanup message sync, only run whole component in new app

--- components/NativeApp/MessageSync.js
+++ components/NativeApp/MessageSync.js
@@ -4,8 +4,7 @@ import gql from 'graphql-tag'
 import { parse } from 'url'
 import { useRouter } from 'next/router'
 
-import withInNativeApp, { postMessage } from '../../lib/withInNativeApp'
-import { parseJSONObject } from '../../lib/safeJSON'
+import { useInNativeApp, postMessage } from '../../lib/withInNativeApp'
 import withMe from '../../lib/apollo/withMe'
 
 import AppSignInOverlay from './AppSignInOverlay'
@@ -30,66 +29,43 @@ const pendingAppSignInQuery = gql`
   }
 `
 
-const MessageSync = ({
-  inNativeApp,
-  inNativeIOSApp,
-  inNativeAppLegacy,
-  upsertDevice,
-  me,
-  client
-}) => {
+const MessageSync = ({ upsertDevice, me, client }) => {
   const [signInQuery, setSignInQuery] = useState()
   const router = useRouter()
-  const inNewApp = inNativeApp && !inNativeAppLegacy
-
-  async function checkPendingAppSignIn() {
-    const {
-      data: { pendingAppSignIn }
-    } = await client.query({
-      query: pendingAppSignInQuery,
-      fetchPolicy: 'network-only'
-    })
-    if (pendingAppSignIn) {
-      const verificationUrlObject = parse(
-        pendingAppSignIn.verificationUrl,
-        true
-      )
-      const { query } = verificationUrlObject
-      setSignInQuery(query)
-    }
-  }
 
   useEffect(() => {
-    if (!inNewApp) {
-      return
-    }
     const handleRouteChange = url => {
-      console.log(`App is changing to ${url}`)
       postMessage({
         type: 'routeChange',
-        // ToDo: replace default "canGoBack: true "
-        payload: { url, canGoBack: true }
+        payload: { url }
       })
     }
     router.events.on('routeChangeComplete', handleRouteChange)
-
-    // If the component is unmounted, unsubscribe with the `off` method:
     return () => {
       router.events.off('routeChangeComplete', handleRouteChange)
     }
-  }, [inNewApp])
-
-  useEffect(() => {
-    if (!me) {
-      return
-    }
-    checkPendingAppSignIn()
-  }, [me])
+  }, [])
 
   const { saveMediaProgress } = useMediaProgress()
   useEffect(() => {
-    if (!inNewApp) {
-      return
+    async function checkPendingAppSignIn() {
+      const {
+        data: { pendingAppSignIn }
+      } = await client.query({
+        query: pendingAppSignInQuery,
+        fetchPolicy: 'network-only'
+      })
+      if (pendingAppSignIn) {
+        const verificationUrlObject = parse(
+          pendingAppSignIn.verificationUrl,
+          true
+        )
+        const { query } = verificationUrlObject
+        setSignInQuery(query)
+      }
+    }
+    if (me) {
+      checkPendingAppSignIn()
     }
     const onMessage = event => {
       const { content = {}, id } = event.data
@@ -137,7 +113,7 @@ const MessageSync = ({
     return () => {
       document.removeEventListener('message', onMessage)
     }
-  }, [inNewApp, inNativeIOSApp, me])
+  }, [me])
 
   if (signInQuery) {
     return (
@@ -152,8 +128,18 @@ const MessageSync = ({
 }
 
 export default compose(
+  WrappedComponent => {
+    const InNewAppOnly = props => {
+      const { inNativeApp, inNativeAppLegacy } = useInNativeApp()
+      if (inNativeApp && !inNativeAppLegacy) {
+        return <WrappedComponent {...props} />
+      }
+      return null
+    }
+
+    return InNewAppOnly
+  },
   withMe,
   graphql(upsertDeviceQuery, { name: 'upsertDevice' }),
-  withApollo,
-  withInNativeApp
+  withApollo
 )(MessageSync)

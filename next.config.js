0b8eb4db18df70f9992624238350c3a853d97a4e
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-04-14 13:24:39 +0200

    bump next.js

--- next.config.js
+++ next.config.js
@@ -5,29 +5,19 @@ const withBundleAnalyzer = require('@next/bundle-analyzer')({
 const { NODE_ENV, CDN_FRONTEND_BASE_URL } = process.env
 
 module.exports = withBundleAnalyzer({
+  future: {
+    webpack5: true
+  },
   webpack: config => {
     config.externals = config.externals || {}
     config.externals['lru-cache'] = 'lru-cache'
     config.externals['react-dom/server'] = 'react-dom/server'
-
-    const entryFactory = config.entry
-    const polyfillPath = './lib/polyfill.js'
 
     const alias = Object.assign({}, config.resolve.alias)
     delete alias.url
     config.resolve = {
       ...config.resolve,
       alias
-    }
-
-    config.entry = async () => {
-      const entries = await entryFactory()
-
-      if (entries['main.js'] && !entries['main.js'].includes(polyfillPath)) {
-        entries['main.js'].unshift(polyfillPath)
-      }
-
-      return entries
     }
     return config
   },

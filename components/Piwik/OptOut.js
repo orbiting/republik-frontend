8133c1e2ebb7af650911f0485e6d90d1c39af951
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-05-05 08:35:42 +0200

    modernize matomo opt out, use new track API

--- components/Piwik/OptOut.js
+++ components/Piwik/OptOut.js
@@ -1,83 +1,67 @@
-import React, { Component } from 'react'
-import { compose } from 'react-apollo'
-import { css } from 'glamor'
+import React, { useCallback, useEffect, useState, useRef } from 'react'
 
-import { Label, A, mediaQueries } from '@project-r/styleguide'
+import { Label, A } from '@project-r/styleguide'
 
 import withT from '../../lib/withT'
-import jsonp from '../../lib/utils/jsonp'
-import { PIWIK_URL_BASE } from '../../lib/constants'
+import track from '../../lib/piwik'
 
 import Anchor from '../Anchor'
 
-class OptOut extends Component {
-  constructor(...args) {
-    super(...args)
-    this.state = {}
-  }
-  change(turn) {
-    this.setState({ status: 'saving' })
-    const action = turn === 'off' ? 'doIgnore' : 'doTrack'
-    jsonp(
-      `${PIWIK_URL_BASE}/index.php?module=API&method=AjaxOptOut.${action}&format=json`,
-      {},
-      (error, data) => {
-        if (error || data.result !== 'success') {
-          this.setState({ status: 'fail' })
-          return
-        }
-        this.loadStatus()
+const OptOut = ({ t }) => {
+  const [status, setStatus] = useState('loading')
+  const timeout = useRef()
+  const loadStatus = useCallback(() => {
+    clearTimeout(timeout.current)
+    timeout.current = setTimeout(() => {
+      setStatus('timeout')
+    }, 15000)
+    track([
+      function() {
+        clearTimeout(timeout.current)
+        setStatus(this.isUserOptedOut() ? 'off' : 'on')
       }
-    )
-  }
-  loadStatus() {
-    this.setState({ status: 'loading' })
-    jsonp(
-      `${PIWIK_URL_BASE}/index.php?module=API&method=AjaxOptOut.isTracked&format=json`,
-      {},
-      (error, data) => {
-        if (error) {
-          this.setState({ status: 'timeout' })
-          return
-        }
-
-        this.setState({ status: data.value ? 'on' : 'off' })
-      }
-    )
-  }
-  componentDidMount() {
+    ])
+  }, [])
+  useEffect(() => {
     if (navigator.doNotTrack) {
-      this.setState({ status: 'dnt' })
-    } else {
-      this.loadStatus()
+      setStatus('dnt')
+      return
     }
-  }
-  render() {
-    const { t } = this.props
-    const { status } = this.state
+    loadStatus()
+    return () => {
+      clearTimeout(timeout.current)
+    }
+  }, [])
 
-    return (
-      <span>
-        <Anchor id='tracking' />
-        <Label>{t('piwik/optOut/label')}</Label>
-        <br />
-        {!!status && t(`piwik/optOut/status/${status}`)}
-        {(status === 'off' || status === 'on') && (
-          <A
-            href='#tracking'
-            onClick={e => {
-              e.preventDefault()
-              this.change(status === 'off' ? 'on' : 'off')
-            }}
-          >
-            {' '}
-            {t(`piwik/optOut/turn/${status === 'off' ? 'on' : 'off'}`)}
-          </A>
-        )}
-        <noscript>{t('piwik/optOut/noscript')}</noscript>
-      </span>
-    )
-  }
+  return (
+    <span>
+      <Anchor id='tracking' />
+      <Label>{t('piwik/optOut/label')}</Label>
+      <br />
+      {!!status && t(`piwik/optOut/status/${status}`)}
+      {(status === 'off' || status === 'on') && (
+        <A
+          href='#tracking'
+          onClick={e => {
+            e.preventDefault()
+            setStatus('saving')
+            if (status === 'off') {
+              // needs double call for loadStatus to switch in Safari
+              track(['forgetUserOptOut'])
+              track(['forgetUserOptOut'])
+            } else {
+              track(['optUserOut'])
+            }
+            loadStatus()
+          }}
+        >
+          {' '}
+          {t(`piwik/optOut/turn/${status === 'off' ? 'on' : 'off'}`)}
+        </A>
+      )}
+      <noscript>{t('piwik/optOut/noscript')}</noscript>
+    </span>
+  )
 }
 
-export default compose(withT)(OptOut)
+export default withT(OptOut)

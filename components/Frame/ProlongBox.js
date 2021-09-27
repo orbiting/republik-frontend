aa1b3e1789ae5cb06f00f2a154335eabf68f5e0d
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-09-27 16:37:27 +0200

    refactor minimal app version detection and add explanation

--- components/Frame/ProlongBox.js
+++ components/Frame/ProlongBox.js
@@ -40,13 +40,11 @@ const dayFormat = timeFormat('%d. %B %Y')
 const ProlongBox = ({ t, prolongBeforeDate, membership }) => {
   const [colorScheme] = useColorContext()
   const router = useRouter()
-  const {
-    inNativeAppWithStripeCompatibility,
-    inNativeIOSApp
-  } = useInNativeApp()
+  const { isMinimalNativeAppVersion, inNativeIOSApp } = useInNativeApp()
 
   if (
-    (inNativeIOSApp && !inNativeAppWithStripeCompatibility) ||
+    // on iOS iframe to stripe were not possible before app v2.0.2
+    (inNativeIOSApp && !isMinimalNativeAppVersion('2.0.2')) ||
     router.pathname === '/angebote' ||
     router.pathname === '/abgang' ||
     router.pathname === '/cockpit'

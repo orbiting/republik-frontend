a3fc8b525af574e813c341b8a6d8749b9e5cd6d3
Tobias Maier <trm217@protonmail.com>
2021-10-01 13:09:45 +0200

    add createStaticProps helper

---
+++ lib/helpers/createGetStaticProps.ts
@@ -0,0 +1,51 @@
+import { ParsedUrlQuery } from 'querystring'
+import {
+  GetStaticProps,
+  GetStaticPropsContext,
+  GetStaticPropsResult
+} from 'next'
+import { BasePageProps } from '../../pages/_app'
+import {
+  APOLLO_STATE_PROP_NAME,
+  initializeApollo
+} from '../apollo/apolloClient'
+import { ApolloClient, NormalizedCacheObject } from '@apollo/client'
+
+/**
+ * A function that is able to interact with the apollo-client
+ */
+type ApolloSSGQueryFunc<P, Q extends ParsedUrlQuery> = (
+  client: ApolloClient<NormalizedCacheObject>,
+  params: Q
+) => Promise<Partial<GetStaticPropsResult<P>>>
+
+/**
+ * createGetStaticProps returns a getStaticProps-function that may fetch data
+ * from the graphql api.
+ * @param queryFunc
+ */
+function createGetStaticProps<
+  P extends BasePageProps,
+  Q extends ParsedUrlQuery = ParsedUrlQuery
+>(queryFunc: ApolloSSGQueryFunc<P, Q>): GetStaticProps<BasePageProps<P>> {
+  return async (
+    ctx: GetStaticPropsContext<Q>
+  ): Promise<GetStaticPropsResult<BasePageProps<P>>> => {
+    const apolloClient = initializeApollo()
+    const result = await queryFunc(apolloClient, ctx.params)
+
+    // TODO: Fix TypeScript warning
+    if (!('notFound' in result || 'redirect' in result)) {
+      result.props = {}
+    }
+
+    // If the result has
+    if ('props' in result) {
+      result.props[APOLLO_STATE_PROP_NAME] = apolloClient.cache.extract()
+    }
+
+    return result
+  }
+}
+
+export default createGetStaticProps

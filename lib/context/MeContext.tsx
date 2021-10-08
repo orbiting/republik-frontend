a30ceaf588d31957890703ecc17fdd5789689a29
Tobias Maier <trm217@protonmail.com>
2021-10-08 09:20:08 +0200

    further improve hiding of paynotes while loading user

--- lib/context/MeContext.tsx
+++ lib/context/MeContext.tsx
@@ -5,8 +5,7 @@ import React, {
   useEffect,
   useMemo
 } from 'react'
-import Head from 'next/head'
-import Script from 'next/script'
+import NextHead from 'next/head'
 import { ApolloError, useQuery } from '@apollo/client'
 import { checkRoles, meQuery } from '../apollo/withMe'
 import { css } from 'glamor'
@@ -15,7 +14,7 @@ const IS_MEMBER_ATTRIBUTE = 'data-is-member'
 const MEMBERSHIP_STORAGE_KEY = 'is-member'
 
 // Rule to hide elements while a statically generated page is fetching the active-user
-css.global(`body[${IS_MEMBER_ATTRIBUTE}="true"] [data-hide-for-member]`, {
+css.global(`[${IS_MEMBER_ATTRIBUTE}="true"] [data-hide-for-member]`, {
   display: 'none'
 })
 
@@ -70,19 +69,21 @@ const MeContextProvider = ({ children }: Props) => {
   const { data, loading, error, refetch } = useQuery<MeResponse>(meQuery, {})
 
   const me = useMemo(() => {
-    if (data) return data.me
+    if (!data) return undefined
+    if (data.me) return undefined
 
     return undefined
   }, [data])
 
   useEffect(() => {
     if (!data) return
-    if (data && data.me) {
-      //document.body.removeAttribute(IS_MEMBER_ATTRIBUTE)
+    if (data.me) {
       localStorage.setItem(
         MEMBERSHIP_STORAGE_KEY,
         String(checkRoles(data.me, ['member']))
       )
+    } else {
+      localStorage.removeItem(MEMBERSHIP_STORAGE_KEY)
     }
   }, [data])
 
@@ -97,17 +98,18 @@ const MeContextProvider = ({ children }: Props) => {
         hasAccess: checkRoles(me, ['member'])
       }}
     >
-      <Script
-        id='hide-elements-if-member-script'
-        dangerouslySetInnerHTML={{
-          __html: `
+      <NextHead>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
           try {
             const value = localStorage.getItem("${MEMBERSHIP_STORAGE_KEY}");
-            document.body.setAttribute("${IS_MEMBER_ATTRIBUTE}", value);
+            document.documentElement.setAttribute("${IS_MEMBER_ATTRIBUTE}", value);
          } catch(e) {console.error(e)}
           `
-        }}
-      />
+          }}
+        />
+      </NextHead>
       {children}
     </MeContext.Provider>
   )

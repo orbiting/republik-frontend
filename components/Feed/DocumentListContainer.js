305a4ab2169d1e50f1135df604324a837fa0e32e
Thomas Preusse <tpreusse@users.noreply.github.com>
2021-11-09 10:33:57 +0100

    refactor document list container to hooks

--- components/Feed/DocumentListContainer.js
+++ components/Feed/DocumentListContainer.js
@@ -1,11 +1,10 @@
-import React, { Component } from 'react'
+import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
-import { Query } from '@apollo/client/react/components'
 import Loader from '../Loader'
 import DocumentList from './DocumentList'
-import noop from 'lodash/noop'
 
 import { documentFragment } from './fragments'
+import { useQuery } from '@apollo/client'
 
 export const documentListQueryFragment = `
   fragment DocumentListConnection on DocumentConnection {
@@ -63,79 +62,71 @@ export const makeLoadMore = ({
     }
   })
 
-class LifecycleWrapper extends Component {
-  componentWillUnmount() {
-    this.props.onComponentWillUnmount && this.props.onComponentWillUnmount()
-  }
-  render() {
-    return this.props.children
-  }
-}
+const DocumentListContainer = props => {
+  const {
+    query,
+    variables,
+    getConnection,
+    mergeConnection,
+    mapNodes,
+    placeholder,
+    help,
+    empty,
+    feedProps,
+    refetchOnUnmount,
+    showTotal
+  } = props
 
-class DocumentListContainer extends Component {
-  render() {
-    const {
-      query,
-      variables,
-      getConnection,
-      mergeConnection,
-      mapNodes,
-      placeholder,
-      help,
-      empty,
-      feedProps,
-      refetchOnUnmount,
-      showTotal
-    } = this.props
+  const { loading, error, data, fetchMore, refetch } = useQuery(query, {
+    variables
+  })
 
-    return (
-      <Query query={query} variables={variables}>
-        {({ loading, error, data, fetchMore, refetch }) => (
-          <LifecycleWrapper
-            onComponentWillUnmount={refetchOnUnmount ? refetch : noop}
-          >
-            <Loader
-              loading={loading}
-              error={error}
-              render={() => {
-                const connection = getConnection(data)
-                const isEmpty = connection.totalCount < 1
-                if (isEmpty) {
-                  return placeholder
-                } else {
-                  const hasMore = connection.pageInfo.hasNextPage
-                  return (
-                    <>
-                      <DocumentList
-                        documents={connection.nodes
-                          .map(mapNodes)
-                          .filter(Boolean)}
-                        totalCount={connection.totalCount}
-                        hasMore={hasMore}
-                        loadMore={makeLoadMore({
-                          fetchMore,
-                          connection,
-                          getConnection,
-                          mergeConnection,
-                          mapNodes,
-                          variables
-                        })}
-                        feedProps={feedProps}
-                        showTotal={showTotal}
-                        help={help}
-                        empty={empty}
-                        variables={variables}
-                      />
-                    </>
-                  )
-                }
-              }}
+  useEffect(() => {
+    if (refetchOnUnmount) {
+      return () => {
+        refetch()
+      }
+    }
+  }, [refetchOnUnmount])
+  if (process.browser) {
+    window.refetch = refetch
+  }
+
+  return (
+    <Loader
+      loading={loading}
+      error={error}
+      render={() => {
+        const connection = getConnection(data)
+        const isEmpty = connection.totalCount < 1
+        if (isEmpty) {
+          return placeholder
+        } else {
+          const hasMore = connection.pageInfo.hasNextPage
+          return (
+            <DocumentList
+              documents={connection.nodes.map(mapNodes).filter(Boolean)}
+              totalCount={connection.totalCount}
+              hasMore={hasMore}
+              loadMore={makeLoadMore({
+                fetchMore,
+                connection,
+                getConnection,
+                mergeConnection,
+                mapNodes,
+                variables
+              })}
+              feedProps={feedProps}
+              showTotal={showTotal}
+              help={help}
+              empty={empty}
+              variables={variables}
             />
-          </LifecycleWrapper>
-        )}
-      </Query>
-    )
-  }
+          )
+        }
+      }}
+    />
+  )
 }
 
 DocumentListContainer.defaultProps = defaultProps

03f84d8e61fb17ead25842bf2753dd38047c3eeb
Olivier Baumann <olivier@iovi.io>
2021-11-09 10:09:04 +0100

    adds back Signinpage for iOS versions below 2.1.0

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,8 @@ import compose from 'lodash/flowRight'
 import { withRouter } from 'next/router'
 
 import Frame from '../components/Frame'
+import { useInNativeApp } from '../lib/withInNativeApp'
+import SignInPage from './anmelden'
 import Front from '../components/Front'
 import Marketing from '../components/Marketing'
 import withT from '../lib/withT'
@@ -13,6 +15,7 @@ import { PUBLIC_BASE_URL, CDN_FRONTEND_BASE_URL } from '../lib/constants'
 import withDefaultSSR from '../lib/hocs/withDefaultSSR'
 
 const IndexPage = ({ t, isMember, router }) => {
+  const { inNativeIOSApp, isMinimalNativeAppVersion } = useInNativeApp()
   if (
     router.query.stale !== 'marketing' &&
     (isMember || router.query.extractId)
@@ -26,6 +29,11 @@ const IndexPage = ({ t, isMember, router }) => {
         finite
       />
     )
+  }
+
+  // only show marketing in ios app if it's the latest version
+  if (inNativeIOSApp && isMinimalNativeAppVersion('2.1.0')) {
+    return <SignInPage />
   }
 
   const meta = {

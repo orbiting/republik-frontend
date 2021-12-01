63aa20320bd5a1fe03c4828720a265c594c1e5fb
Tobias Maier <trm217@protonmail.com>
2021-12-01 19:56:07 +0100

    undo commit

--- components/Discussion/Discussion.js
+++ components/Discussion/Discussion.js
@@ -5,16 +5,113 @@ import DiscussionCommentComposer from './DiscussionCommentComposer'
 import { GENERAL_FEEDBACK_DISCUSSION_ID } from '../../lib/constants'
 import Comments from './Comments'
 import TagFilter from './TagFilter'
+import { withDiscussionComments } from './graphql/enhancers/withDiscussionComments'
 import Loader from '../Loader'
-import { useQuery } from '@apollo/client'
-import { commentsSubscription, discussionQuery } from './graphql/documents'
-import { debug } from './debug'
-import produce from '../../lib/immer'
-import { bumpCounts, mergeComment, mergeComments } from './graphql/store'
 
 const DEFAULT_DEPTH = 3
 
 const Discussion = ({
+  discussionComments,
+  discussionId,
+  focusId,
+  meta,
+  board,
+  parent,
+  parentId,
+  includeParent,
+  rootCommentOverlay,
+  showPayNotes,
+  orderBy,
+  activeTag,
+  depth
+}) => {
+  return (
+    <div data-discussion-id={discussionId}>
+      {!rootCommentOverlay && (
+        <>
+          <TagFilter discussion={discussionComments.discussion} />
+          <DiscussionCommentComposer
+            discussionComments={discussionComments}
+            discussionId={discussionId}
+            parentId={parentId}
+            orderBy={orderBy}
+            depth={depth}
+            focusId={focusId}
+            includeParent={includeParent}
+            activeTag={activeTag}
+            showPayNotes={showPayNotes}
+          />
+        </>
+      )}
+
+      <div style={{ margin: rootCommentOverlay ? 0 : '20px 0' }}>
+        <Comments
+          key={
+            `${orderBy}-${activeTag ||
+              'all'}` /* To remount of the whole component on change */
+          }
+          discussionComments={discussionComments}
+          discussionId={discussionId}
+          parentId={parentId}
+          orderBy={orderBy}
+          depth={depth}
+          focusId={board ? undefined : focusId}
+          includeParent={includeParent}
+          activeTag={activeTag}
+          meta={meta}
+          board={board}
+          parent={board ? parent || focusId : undefined}
+          rootCommentOverlay={rootCommentOverlay}
+        />
+      </div>
+    </div>
+  )
+}
+
+const DiscussionLoader = withDiscussionComments(
+  ({
+    discussionComments,
+    discussionId,
+    focusId = null,
+    meta,
+    board,
+    parent,
+    parentId = null,
+    includeParent,
+    rootCommentOverlay,
+    showPayNotes,
+    orderBy,
+    activeTag,
+    depth
+  }) => (
+    <Loader
+      loading={discussionComments.loading}
+      error={discussionComments.error}
+      render={() => {
+        if (!discussionComments.discussion) return null
+        return (
+          <Discussion
+            discussionComments={discussionComments}
+            discussionId={discussionId}
+            focusId={focusId}
+            meta={meta}
+            board={board}
+            parent={parent}
+            parentId={parentId}
+            includeParent={includeParent}
+            rootCommentOverlay={rootCommentOverlay}
+            showPayNotes={showPayNotes}
+            orderBy={orderBy}
+            activeTag={activeTag}
+            depth={depth}
+          />
+        )
+      }}
+    />
+  )
+)
+
+const DiscussionWrapper = ({
   discussionId,
   focusId = null,
   meta,
@@ -42,155 +139,22 @@ const Discussion = ({
 
   const depth = board ? 1 : DEFAULT_DEPTH
 
-  const { data, error, loading, fetchMore, subscribeToMore } = useQuery(
-    discussionQuery,
-    {
-      variables: {
-        discussionId,
-        orderBy,
-        activeTag,
-        depth: depth,
-        includeParent
-      }
-    }
-  )
-
-  const initialParentId = parentId
-
-  const discussionComments = {
-    ...data,
-    fetchMore: ({
-      parentId,
-      after,
-      appendAfter,
-      depth,
-      includeParent
-    } = {}) => {
-      return fetchMore({
-        variables: {
-          discussionId,
-          parentId,
-          after,
-          orderBy,
-          activeTag,
-          depth: depth,
-          includeParent
-        },
-        updateQuery: (previousResult, { fetchMoreResult: { discussion } }) => {
-          return produce(
-            previousResult,
-            mergeComments({
-              parentId,
-              appendAfter,
-              comments: discussion.comments
-            })
-          )
-        }
-      })
-    },
-    subscribe: () => {
-      return subscribeToMore({
-        document: commentsSubscription,
-        variables: { discussionId },
-        onError(...args) {
-          debug('subscribe:onError', args)
-        },
-        updateQuery: (previousResult, { subscriptionData }) => {
-          debug('subscribe:updateQuery', subscriptionData.data)
-
-          /*
-           * Regardless of what we do here, the Comment object in the cache will be updated.
-           * We only have to take care of updating objects other than the Comment, like in
-           * this case update the Discussion object. This is why we only care about the
-           * 'CREATED' mutation and ignore 'DELETED' (which can't happen anyways) and 'UPDATED'.
-           */
-          if (
-            subscriptionData.data &&
-            subscriptionData.data.comment.mutation === 'CREATED'
-          ) {
-            const comment = subscriptionData.data.comment.node
-
-            if (
-              initialParentId &&
-              !comment.parentIds.includes(initialParentId)
-            ) {
-              return previousResult
-            }
-
-            /*
-             * Ignore updates related to comments we created in the current client session.
-             * If this is the first comment in the discussion, show it immediately. Otherwise
-             * just bump the counts and let the user click the "Load More" buttons.
-             */
-            if (previousResult.discussion.comments.totalCount === 0) {
-              return produce(
-                previousResult,
-                mergeComment({ comment, initialParentId })
-              )
-            } else {
-              return produce(
-                previousResult,
-                bumpCounts({ comment, initialParentId })
-              )
-            }
-          } else {
-            return previousResult
-          }
-        }
-      })
-    }
-  }
-
   return (
-    <Loader
-      loading={loading}
-      error={error}
-      render={() => {
-        if (!discussionComments.discussion) return null
-        return (
-          <div data-discussion-id={discussionId}>
-            {!rootCommentOverlay && (
-              <>
-                <TagFilter discussion={discussionComments.discussion} />
-                <DiscussionCommentComposer
-                  discussionComments={discussionComments}
-                  discussionId={discussionId}
-                  parentId={parentId}
-                  orderBy={orderBy}
-                  depth={depth}
-                  focusId={focusId}
-                  includeParent={includeParent}
-                  activeTag={activeTag}
-                  showPayNotes={showPayNotes}
-                />
-              </>
-            )}
-
-            <div style={{ margin: rootCommentOverlay ? 0 : '20px 0' }}>
-              <Comments
-                key={
-                  `${orderBy}-${activeTag ||
-                    'all'}` /* To remount of the whole component on change */
-                }
-                discussionComments={discussionComments}
-                discussionId={discussionId}
-                parentId={parentId}
-                orderBy={orderBy}
-                depth={depth}
-                focusId={board ? undefined : focusId}
-                includeParent={includeParent}
-                activeTag={activeTag}
-                meta={meta}
-                board={board}
-                parent={board ? parent || focusId : undefined}
-                rootCommentOverlay={rootCommentOverlay}
-              />
-            </div>
-          </div>
-        )
-      }}
+    <DiscussionLoader
+      discussionId={discussionId}
+      focusId={focusId}
+      meta={meta}
+      board={board}
+      parent={parent}
+      parentId={parentId}
+      includeParent={includeParent}
+      rootCommentOverlay={rootCommentOverlay}
+      showPayNotes={showPayNotes}
+      orderBy={orderBy}
+      activeTag={activeTag}
+      depth={depth}
     />
   )
 }
 
-export default Discussion
+export default DiscussionWrapper

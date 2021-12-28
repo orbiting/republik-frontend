925562745bf4ef6373edd5d8c30b3cc950977465
Tobias Maier <trm217@protonmail.com>
2021-12-28 11:55:52 +0100

    add logic to share comment

--- components/StatementsDiscussion/StatementDiscussion.js
+++ components/StatementsDiscussion/StatementDiscussion.js
@@ -6,7 +6,9 @@ import StatementComposer from './StatementComposer'
 import withT from '../../lib/withT'
 import TagFilter from '../Discussion/TagFilter'
 import OrderByTabs from '../Discussion/OrderByTabs'
-import { DiscussionContext } from '../Discussion/DiscussionProvider/context/DiscussionContext'
+import { useDiscussion } from '../Discussion/DiscussionProvider/context/DiscussionContext'
+import { postMessage, useInNativeApp } from '../../lib/withInNativeApp'
+import { getFocusUrl } from '../Discussion/CommentLink'
 
 const StatementDiscussion = ({ t, tagMappings }) => {
   const {
@@ -16,9 +18,11 @@ const StatementDiscussion = ({ t, tagMappings }) => {
     refetch,
     actions,
     fetchMore,
-    orderBy
-  } = useContext(DiscussionContext)
+    orderBy,
+    overlays: { shareOverlay }
+  } = useDiscussion()
   const { me } = useMe()
+  const { inNativeApp } = useInNativeApp()
 
   const filteredStatements = useMemo(
     () =>
@@ -39,6 +43,25 @@ const StatementDiscussion = ({ t, tagMappings }) => {
       after: endCursor,
       appendAfter: lastNode.id
     })
+  }
+
+  const shareComment = async comment => {
+    if (inNativeApp) {
+      postMessage({
+        type: 'share',
+        payload: {
+          title: discussion.title,
+          url: getFocusUrl(discussion, comment),
+          subject: t('discussion/share/emailSubject', {
+            title: discussion.title
+          }),
+          dialogTitle: t('article/share/title')
+        }
+      })
+    } else {
+      shareOverlay.handleOpen(getFocusUrl(discussion, comment))
+    }
+    return Promise.resolve({ ok: true })
   }
 
   return (
@@ -69,7 +92,8 @@ const StatementDiscussion = ({ t, tagMappings }) => {
               actions={{
                 handleUpVote: actions.handleUpVote,
                 handleDownVote: actions.handleDownVote,
-                handleUnVote: actions.handleUnVote
+                handleUnVote: actions.handleUnVote,
+                handleShare: shareComment
               }}
               disableVoting={!me && !discussion.userCanComment}
               loadMore={loadMore}

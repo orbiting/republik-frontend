7a6b5eb5641413ad8c3ae9c7b6793215e35ca1f0
Tobias Maier <trm217@protonmail.com>
2022-01-04 17:25:29 +0100

    add wrapper for ssr article-page

--- pages/_ssr/[...path].tsx
+++ pages/_ssr/[...path].tsx
@@ -1,7 +1,8 @@
-import ArticlePage from '../[...path]'
+import React from 'react'
+import Article from '../../components/Article/Page'
 import withDefaultSSR from '../../lib/hocs/withDefaultSSR'
 
 /**
  * Reexport the article page with SSR-mode.
  */
-export default withDefaultSSR(ArticlePage)
+export default withDefaultSSR(props => <Article {...props} />)

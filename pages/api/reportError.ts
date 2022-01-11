c2bc5acde5ade1d0a411e6178267a7453ef7b092
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-01-11 10:28:29 +0100

    Revert "add helper to handle only whitelisted httpMethods"
    This reverts commit f49372889d96e7cb02f7bf6e6b397df0f3b8f74f.

--- pages/api/reportError.ts
+++ pages/api/reportError.ts
@@ -1,23 +1,23 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import chalk from 'chalk'
 import useragent from 'useragent'
-import {
-  HTTPMethod,
-  withAcceptedMethods
-} from '../../lib/helpers/APIHandler.helper'
 
-function handler(req: NextApiRequest, res: NextApiResponse): void {
-  console.warn(
-    chalk.yellow(
-      'reportError from',
-      // TODO: replace useragent dependency
-      useragent(req.headers['user-agent']),
-      req.body
+export default (req: NextApiRequest, res: NextApiResponse): void => {
+  if (req.method === 'POST') {
+    console.warn(
+      chalk.yellow(
+        'reportError from',
+        // TODO: replace useragent dependency
+        useragent(req.headers['user-agent']),
+        req.body
+      )
     )
-  )
-  return res.status(200).send({
-    ack: true
+    return res.status(200).send({
+      ack: true
+    })
+  }
+
+  return res.status(405).json({
+    message: 'Method not Allowed'
   })
 }
-
-export default withAcceptedMethods(handler, [HTTPMethod.POST])

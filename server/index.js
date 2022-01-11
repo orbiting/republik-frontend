6d314c97a980142f6b91ec95c4f7c362b7110229
Thomas Preusse <tpreusse@users.noreply.github.com>
2022-01-11 10:28:31 +0100

    Revert "remove rewriting post to get on /en"
    This reverts commit 535cf0295b2f5b15cbd1137b10d52ce265ca8675.

--- server/index.js
+++ server/index.js
@@ -132,6 +132,11 @@ app.prepare().then(() => {
     return app.render(req, res, '/503', req.query)
   })
 
+  // PayPal donate return url can be posted to
+  server.post('/en', (req, res) => {
+    return app.render(req, res, '/en', req.query)
+  })
+
   // Report Error
   server.post('/api/reportError', bodyParser.text(), (req, res) => {
     console.warn(

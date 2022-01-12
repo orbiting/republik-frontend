08f6bc5bbc61fa3516d9b99b9132a27c500aa570
Tobias Maier <trm217@protonmail.com>
2022-01-12 15:01:13 +0100

    move report-comment action into useReportComment-handler hook

--- components/Discussion/DiscussionProvider/hooks/useDiscussionMutations.ts
+++ components/Discussion/DiscussionProvider/hooks/useDiscussionMutations.ts
@@ -1,18 +1,13 @@
 import { useMutation } from '@apollo/client'
-import {
-  REPORT_COMMENT_MUTATION,
-  UNPUBLISH_COMMENT_MUTATION
-} from '../../graphql/documents'
+import { UNPUBLISH_COMMENT_MUTATION } from '../../graphql/documents'
 import { toRejectedString } from '../../graphql/utils'
 
 export type DiscussionMutations = {
   unpublishCommentHandler: any
-  reportCommentHandler: any
 }
 
 function useDiscussionMutations(): DiscussionMutations {
   const [unpublishCommentMutation] = useMutation(UNPUBLISH_COMMENT_MUTATION)
-  const [reportCommentMutation] = useMutation(REPORT_COMMENT_MUTATION)
 
   // TODO: Implement with overlay
   //const [featureCommentMutation] = useMutation(FEATURE_COMMENT_MUTATION)
@@ -25,19 +20,8 @@ function useDiscussionMutations(): DiscussionMutations {
     }).catch(toRejectedString)
   }
 
-  function reportCommentHandler(commentId) {
-    return reportCommentMutation({
-      variables: {
-        commentId: commentId
-      }
-    }).catch(toRejectedString)
-  }
-
-  // TODO: Feature comment
-
   return {
-    unpublishCommentHandler,
-    reportCommentHandler
+    unpublishCommentHandler
   }
 }
 

6676e8f6bd307c0642943ff29039a4a0c6950d82
Tobias Maier <trm217@protonmail.com>
2022-01-14 11:03:15 +0100

    move CommentNodeWrapper and start implementation

--- components/Discussion/CommentNodeWrapper/CommentNodeWrapper.tsx
+++ components/Discussion/CommentNodeWrapper/CommentNodeWrapper.tsx
@@ -1,15 +1,15 @@
 import React, { useMemo, useState } from 'react'
 import { CommentNode } from '@project-r/styleguide'
-import { CommentFragmentType } from '../DiscussionProvider/graphql/fragments/CommentFragment.graphql'
 import { useTranslation } from '../../../lib/withT'
 import Link from 'next/link'
 import { getFocusHref } from '../CommentLink'
 import { format } from 'url'
 import { useDiscussion } from '../DiscussionProvider/context/DiscussionContext'
 import useVoteCommentHandlers from '../DiscussionProvider/hooks/actions/useVoteCommentHandlers'
+import { CommentTreeNode } from '../DiscussionProvider/helpers/makeCommentTree'
 
 type Props = {
-  comment: CommentFragmentType
+  comment: CommentTreeNode
 }
 
 const CommentNodeWrapper = ({ comment }: Props) => {
@@ -30,8 +30,16 @@ const CommentNodeWrapper = ({ comment }: Props) => {
     return format(urlObject)
   }, [discussion, comment])
 
+  const profileHref = useMemo(
+    () =>
+      comment?.displayAuthor?.slug
+        ? `/~${comment.displayAuthor.slug}`
+        : focusHref,
+    [comment?.displayAuthor?.slug, focusHref]
+  )
+
   if (isEditing) {
-    return 'Edit-Composer'
+    return <>Hello</>
   }
 
   return (
@@ -40,19 +48,23 @@ const CommentNodeWrapper = ({ comment }: Props) => {
       comment={comment}
       Link={Link}
       focusHref={focusHref}
+      profileHref={profileHref}
       actions={{
         handleUpVote: voteHandlers.upVoteCommentHandler,
         handleDownVote: voteHandlers.downVoteCommentHandler,
         handleUnVote: voteHandlers.unVoteCommentHandler,
         handleReply: () => setIsReplying(true),
-        handleLoadReplies: () => alert('TODO'),
-        handleShare: () => shareOverlay.shareHandler(comment)
+        // handleLoadReplies: () => alert('TODO'),
+        handleShare: shareOverlay.shareHandler
       }}
       menuItems={[]}
       userCanComment={discussion?.userCanComment}
       userWaitUntil={discussion?.userWaitUntil}
     >
       {isReplying && 'Reply-Composer'}
+      {comment.comments.nodes.map(reply => (
+        <CommentNodeWrapper key={reply.id} comment={reply} />
+      ))}
     </CommentNode>
   )
 }
